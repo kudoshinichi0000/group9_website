0bb2af21f71cfa6990a7db50a35140c91f075646
agapito <agapitojezreel25@gmail.com>
2021-04-16 22:34:21 +0800

    poop

--- CostumerService/PrivacyPolicy.php
+++ CostumerService/PrivacyPolicy.php
@@ -5,17 +5,17 @@
     <title>Privacy Policy</title>
   </head>
   <body>
-    <?php include_once "navbar.php"; ?>
+    <?php include_once "group9_website/navbar.php"; ?>
       <div class="Container">
           <h3>
             <h1>Privacy Policy</h1>
-            Your privacy is important to us. It is Feedopedia's policy to <br>
-            respect your privacy and comply with any applicable law and <br>
-            regulation regardingany personal information we may collect about <br>
-            you, including across our <br>
-            website, https://localhost/Php/GroupProj/main.php, <br>
-            and other sites we own and operate.This policy is effective as <br>
-            of 16 April 2021 and was last updated on 16 April 2021. <br>
+            Your privacy is important to us. It is Feedopedia's policy to
+            respect your privacy and comply with any applicable law and
+            regulation regardingany personal information we may collect about
+            you, including across our
+            website, https://localhost/Php/GroupProj/main.php,
+            and other sites we own and operate.This policy is effective as
+            of 16 April 2021 and was last updated on 16 April 2021.
           </h3>
       </div>
   </body>
