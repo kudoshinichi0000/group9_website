3821276907e1ff17533c301c3f2174bdcdcf4100
agapito <agapitojezreel25@gmail.com>
2021-04-16 22:04:40 +0800

    ...

---
+++ CostumerService/PrivacyPolicy.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Privacy Policy</title>
+  </head>
+  <body>
+    <?php include_once "navbar.php"; ?>
+      <div class="Container">
+          <h3>
+            Privacy Policy
+            Your privacy is important to us. It is Feedopedia's policy to
+            respect your privacy and comply with any applicable law and
+            regulation regardingany personal information we may collect about
+            you, including across our
+            website, https://localhost/Php/GroupProj/main.php,
+            and other sites we own and operate.This policy is effective as
+            of 16 April 2021 and was last updated on 16 April 2021.
+          </h3>
+      </div>
+  </body>
+  <?php include_once "footer.php";?>
+</html>
