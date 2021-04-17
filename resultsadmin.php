51adeb8e1b0b0b357e2f808029e4ff61ab318865
agapito <agapitojezreel25@gmail.com>
2021-04-17 11:21:35 +0800

    yey

--- resultsadmin.php
+++ resultsadmin.php
@@ -8,25 +8,11 @@
 	<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
 </head>
 <body>
-	<?php include_once "db.php"; 
+	<?php include_once "db.php";
 	include_once "navbaradmin.php" ?>
 	<?php echo $_SESSION['userid'];?>
-	
+
 	<h1>admin bbbb</h1>
 </body>
-	<!---Footer---->
-	<footer>
-		<br><br>
-		<div class="Centeredtext">
-			<a href="#">Home</a>
-			<a href="#">About</a>
-			<a href="#">Services</a>
-			<a href="#">Contact</a>
-			<a href="#">Terms</a>
-			<a href="#">Data Policy</a>
-			<a href="#">Cookies Policy</a>
-		<br>
-		</div>
-		<div class="cenocolor"><h5>Copyright © 2021 GROUP9</h5></div>
-	</footer>
+	<?php include_once "footer.php";?>
 </html>
