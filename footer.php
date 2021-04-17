3397677bfb837dbfeca2181310c0d0d7cc7b45c9
agapito <agapitojezreel25@gmail.com>
2021-04-17 10:43:38 +0800

    sabog na ko

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 	<title></title>
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<link rel="stylesheet" type="text/css" href="css/navbar.css">
@@ -37,17 +36,20 @@
 						<li><a href="#"><i class="fa fa-angle-double-right"></i>Terms</a></li>
 					</ul>
 				</div>	<div class="col-xs-12 col-sm-5 col-md-2">
-				    <h5>Policies</h5>
-					<ul class="list-unstyled s-links">
-						<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i>Privacy Policy</a></li>
-					</ul>
-					<ul class="list-unstyled s-links">
-						<li><a href="#"><i class="fa fa-angle-double-right"></i> Cookies Policy</a></li>
-					</ul>
-
+					<div class="dropdown">
+						<button class="dropbotton"><h5>Policies</h5></button>
+						<div class="dropdown-content">
+							<ul class="list-unstyled s-links">
+								<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i>Privacy Policy</a></li>
+							</ul>
+							<ul class="list-unstyled s-links">
+								<li><a href="#"><i class="fa fa-angle-double-right"></i> Cookies Policy</a></li>
+							</ul>
+						</div>
+					</div>
 				</div>
-
 			</div>
+			
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
 					<p class="h6">Copyright ©2021 GROUP9 </p>
