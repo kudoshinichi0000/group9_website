a1c4ac57aa7dfac4bc251c264d1751ccacc346b2
agapito <agapitojezreel25@gmail.com>
2021-04-18 21:05:21 +0800

    Henlo

--- footer.php
+++ footer.php
@@ -14,38 +14,55 @@
 	<section id="footer">
 		<div class="container">
 			<div class="row text-center text-xs-center text-sm-left text-md-left">
+
 				<div class="col-xs-12 col-sm-5 col-md-2">
 					<ul class="list-unstyled s-links">
 						<li><a href="main.php"><i class="fa fa-angle-double-right"></i>Home</a></li>
 					</ul>
 				</div>
-					<div class="col-xs-12 col-sm-5 col-md-2">
+
+				<div class="col-xs-12 col-sm-5 col-md-2">
 					<ul class="list-unstyled s-links">
 						<li><a href="#"><i class="fa fa-angle-double-right"></i>About</a></li>
 					</ul>
-				</div>	<div class="col-xs-12 col-sm-5 col-md-2">
+				</div>
+
+				<div class="col-xs-12 col-sm-5 col-md-2">
 					<ul class="list-unstyled s-links">
 						<li><a href="#"><i class="fa fa-angle-double-right"></i>Services</a></li>
 					</ul>
-				</div>	<div class="col-xs-12 col-sm-5 col-md-2">
+				</div>
+
+				<div class="col-xs-12 col-sm-5 col-md-2">
 					<ul class="list-unstyled s-links">
 						<li><a href="Contact.php"><i class="fa fa-angle-double-right"></i>Contact</a></li>
 					</ul>
-				</div>	<div class="col-xs-12 col-sm-5 col-md-2">
+				</div>
+
+				<div class="col-xs-12 col-sm-5 col-md-2">
 					<ul class="list-unstyled s-links">
 						<li><a href="#"><i class="fa fa-angle-double-right"></i>Terms</a></li>
 					</ul>
-				</div>	<div class="col-xs-12 col-sm-5 col-md-2">
-				    <h5>Policies</h5>
+				</div>
+
+				<div class="col-xs-12 col-sm-5 col-md-2">
 					<ul class="list-unstyled s-links">
 						<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i>Privacy Policy</a></li>
 					</ul>
+				</div><br>
+
+				<div class="col-xs-12 col-sm-5 col-md-2">
 					<ul class="list-unstyled s-links">
-						<li><a href="#"><i class="fa fa-angle-double-right"></i> Cookies Policy</a></li>
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>Cookies Policy</a></li>
 					</ul>
 				</div>
-			</div>
 
+				<div class="col-xs-12 col-sm-5 col-md-2">
+					<ul class="list-unstyled s-links">
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>Terms</a></li>
+					</ul>
+				</div>
+				
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
 					<p class="h6">Copyright ©2021 GROUP9 </p>
