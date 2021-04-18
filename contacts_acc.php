3d4f7a447e0f373fa35d947f0ba0ef0562ce3980
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-19 01:32:45 +0800

    km

--- contacts_acc.php
+++ contacts_acc.php
@@ -7,33 +7,32 @@
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
-  <ul>
-    <li>
-      <a href="https://www.facebook.com/JezreelAgapito27">
-        <i class="fa fa-facebook" aria-hidden="true"></i>
-        <span>-Facebook</span>
-      </a>
-    </li>
-    <li>
-      <a href="https://twitter.com/Aakaishuichii">
-        <i class="fa fa-twitter" aria-hidden="true"></i>
-        <span>-Twitter</span>
-      </a>
-    </li>
-    <li>
-    <a href="#">
-      <i class="fa fa-google-plus" aria-hidden="true"></i>
-      <span> - Google</span>
-    </a>
-   </li>
-   <li>
-      <a href="#">
-        <i class="fa fa-instagram" aria-hidden="true"></i>
-        <span> - Instagram</span>
-     </a>
-   </li>
-
 </body>
+<ul>
+  <li>
+    <a href="https://www.facebook.com/JezreelAgapito27">
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+      <span>-Facebook</span>
+    </a>
+  </li>
+  <li>
+    <a href="https://twitter.com/Aakaishuichii">
+      <i class="fa fa-twitter" aria-hidden="true"></i>
+      <span>-Twitter</span>
+    </a>
+  </li>
+  <li>
+  <a href="#">
+    <i class="fa fa-google-plus" aria-hidden="true"></i>
+    <span> - Google</span>
+  </a>
+ </li>
+ <li>
+    <a href="#">
+      <i class="fa fa-instagram" aria-hidden="true"></i>
+      <span> - Instagram</span>
+   </a>
+ </li>
 
-<?php include_once "footer.php"; ?>
+
 </html>
