519b2acff5b15bf82d530abbcf8186fac2483a58
agapito <agapitojezreel25@gmail.com>
2021-04-18 21:09:01 +0800

    hii

--- footer.php
+++ footer.php
@@ -11,27 +11,17 @@
 <body>
 
 </body>
-<<<<<<< HEAD
+
 <!-- Footer -->
 	<section id="footer">
 		<div class="container">
 			<div class="row text-center text-xs-center text-sm-left text-md-left">
 
 				<div class="col-xs-12 col-sm-5 col-md-2">
-=======
-<!-- footer -->
-
-<section id="footer">
-
-	<div class="container">
-			<div class=" row text-center text-xs-center text-sm-left text-md-left">
-					<div class="col-xs-12 col-sm-5 col-md-2">
->>>>>>> 70398a2f7b7a6488094573e7e0f7e6b8b67dc65e
 					<ul class="list-unstyled s-links">
 						<li><a href="main.php"><i class="fa fa-angle-double-right"></i>Home</a></li>
 					</ul>
 				</div>
-<<<<<<< HEAD
 
 				<div class="col-xs-12 col-sm-5 col-md-2">
 					<ul class="list-unstyled s-links">
@@ -74,50 +64,8 @@
 						<li><a href="#"><i class="fa fa-angle-double-right"></i>Terms</a></li>
 					</ul>
 				</div>
-				
+
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
 					<p class="h6">Copyright ©2021 GROUP9 </p>
-=======
-					<div class="col-xs-12 col-sm-5 col-md-2">
-						<ul class="list-unstyled s-links">
-							<li><a href="#"><i class="fa fa-angle-double-right"></i> About </a></li>
-						</ul>
-					</div>
-					<div class="col-xs-12 col-sm-5 col-md-2">
-						<ul class="list-unstyled s-links">
-							<li><a href="#"><i class="fa fa-angle-double-right"></i>Services</a></li>
-						</ul>
-					</div>
-					<div class="col-xs-12 col-sm-5 col-md-2">
-						<ul class="list-unstyled s-links">
-							<li><a href="Contact.php"><i class="fa fa-angle-double-right"></i>Contact</a></li>
-						</ul>
-					</div>
-					<div class="col-xs-12 col-sm 5 col-md-2">
-						<ul class="list-unstyled s-links">
-							<li><a href="#"><i class="fa fa-angle-double-right"></i>Terms</a></li>
-						</ul>
-					</div>
-					<div class="col-xs-12 col-sm 5 colmd-2">
-						<h5>Policies</h5>
-						<ul class="list-unstyled s-links">
-							<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i> PrivacyPolicy</a></li>
-						</ul>
-						<ul class="list-unstyled s-links">
-							<li><a href="#"><i class="fa fa-angle-double-right"></i> Cookies Policy</a></li>
-						</ul>
-					</div>
-
-					<div class="row">
-						<div class="col-x-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text white">
-							<p class="h6">Copyright ©2021 GROUP9</p>
-						</div>
-						<hr>
-					</div>
->>>>>>> 70398a2f7b7a6488094573e7e0f7e6b8b67dc65e
-				</div>
-			</section>
-			<!--footer-->
-
 </html>
