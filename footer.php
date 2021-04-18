925b8beae403b1b7fbd5aa8b37d04b7fd45882a9
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-18 09:05:52 +0800

    yoyo

--- footer.php
+++ footer.php
@@ -2,56 +2,22 @@
 <html>
 <head>
 	<title></title>
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<link rel="stylesheet" type="text/css" href="css/navbar.css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
 
 </body>
-<!-- Footer -->
-	<section id="footer">
-		<div class="container">
-			<div class="row text-center text-xs-center text-sm-left text-md-left">
-				<div class="col-xs-12 col-sm-5 col-md-2">
-					<ul class="list-unstyled s-links">
-						<li><a href="main.php"><i class="fa fa-angle-double-right"></i>Home</a></li>
-					</ul>
-				</div>
-					<div class="col-xs-12 col-sm-5 col-md-2">
-					<ul class="list-unstyled s-links">
-						<li><a href="#"><i class="fa fa-angle-double-right"></i>About</a></li>
-					</ul>
-				</div>	<div class="col-xs-12 col-sm-5 col-md-2">
-					<ul class="list-unstyled s-links">
-						<li><a href="#"><i class="fa fa-angle-double-right"></i>Services</a></li>
-					</ul>
-				</div>	<div class="col-xs-12 col-sm-5 col-md-2">
-					<ul class="list-unstyled s-links">
-						<li><a href="Contact.php"><i class="fa fa-angle-double-right"></i>Contact</a></li>
-					</ul>
-				</div>	<div class="col-xs-12 col-sm-5 col-md-2">
-					<ul class="list-unstyled s-links">
-						<li><a href="#"><i class="fa fa-angle-double-right"></i>Terms</a></li>
-					</ul>
-				</div>	<div class="col-xs-12 col-sm-5 col-md-2">
-				    <h5>Policies</h5>
-					<ul class="list-unstyled s-links">
-						<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i>Privacy Policy</a></li>
-					</ul>
-					<ul class="list-unstyled s-links">
-						<li><a href="#"><i class="fa fa-angle-double-right"></i> Cookies Policy</a></li>
-					</ul>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p class="h6">Copyright ©2021 GROUP9 </p>
-				</div>
-				<hr>
-			</div>
+<footer>
+		<br><br>
+		<div class="Centeredtext">
+			<a href="#">Home</a>
+			<a href="#">About</a>
+			<a href="#">Services</a>
+			<a href="#">Contact</a>
+			<a href="#">Terms</a>
+			<a href="#">Data Policy</a>
+			<a href="#">Cookies Policy</a>
+		<br>
 		</div>
-	</section>
-	<!-- Footer -->
+		<div class="cenocolor"><h5>Copyright © 2021 GROUP9</h5></div>
+	</footer>
+</html>
