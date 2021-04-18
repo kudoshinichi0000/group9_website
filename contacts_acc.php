11414ce76b6d55b5cee7a17350d6ce80be070934
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-18 23:47:48 +0800

    weh

--- contacts_acc.php
+++ contacts_acc.php
@@ -2,7 +2,8 @@
 <html>
 <head>
   <title></title>
-  <link rel="stylesheet" type="text/css" href="css/soc.css">
+  <link rel="stylesheet" type="text/css" href="css/accs.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 </head>
 <body>
 </body>
