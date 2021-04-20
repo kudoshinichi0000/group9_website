7910c35232a318c1ec28150914b5c3adf5e2cd80
agapito <agapitojezreel25@gmail.com>
2021-04-20 23:54:03 +0800

    antok na ko

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <header>
   <div class="container">
-    <img src="css/images/Logo.png" alt="" height="50" style="float: left; padding-top: 0.5em;">
+    <!---<img src="css/images/Logo.png" alt="" height="50" style="float: left; padding-top: 0.5em;">--->
       <div class="logo"><h3>Feedopedia</h3></div>
         <nav>
           <ul>
