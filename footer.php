0687f2bdbfbc858f87be3c1850c063bdb82e9def
agapito <agapitojezreel25@gmail.com>
2021-04-22 02:03:43 +0800

    awdaw

--- footer.php
+++ footer.php
@@ -39,8 +39,8 @@
 					<ul class="list-unstyled s-links">
 						<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i>Cookies Policy</a></li>
 					</ul>
-				</div></div>
-				
+				</div></div><br>
+
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
 					<p class="h6">Copyright ©2021 GROUP9 </p>
