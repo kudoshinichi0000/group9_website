0b9fea32359c2e7f0b65424783c36b9df3a8bc35
agapito <agapitojezreel25@gmail.com>
2021-04-22 00:56:06 +0800

    awdaw

--- main.php
+++ main.php
@@ -6,7 +6,7 @@
 
 	<!---This gives cursive font style to the header--->
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Orelega+One&display=swap" rel="stylesheet">
 
 </head>
 <body>
