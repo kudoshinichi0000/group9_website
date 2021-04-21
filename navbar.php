4c8bced5cec6f52ea88bc0a66d8b4d2938ef6529
agapito <agapitojezreel25@gmail.com>
2021-04-22 00:55:18 +0800

    awdawd

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <header>
   <div class="container">
     <img src="css/images/Logo.png" alt="" height="50" style="float: left; padding-top: 0.5em;">
-      <div class="TextDesign">Feedopedia</div>
+      <div class="TextDesign"><h3>Feedopedia</h3></div>
         <nav>
           <ul>
             <li>
