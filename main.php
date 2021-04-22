14bb656e968528ac192785ad3973b9660e2d1fe2
agapito <agapitojezreel25@gmail.com>
2021-04-22 09:25:06 +0800

    yey

--- main.php
+++ main.php
@@ -18,16 +18,16 @@
 <!--- Lalagyan ng mga <picture> -->
 	<div class="carousel-container">
 		<div class="carousel-slide">
-			<img src="images/Pic1.jpg" alt="img">
-			<img src="images/Pic2.jpg" alt="">
-			<img src="images/Pic3.jpg" alt="">
-			<img src="images/Pic4.jpg" alt="">
-			<img src="images/Pic5.jpeg" alt="">
-			<img src="images/Pic6.jpg" alt="">
-			<img src="images/Pic7.jpg" alt="">
-			<img src="images/Pic8.jpg" alt="">
-			<img src="images/Pic9.jpg" alt="">
-			<img src="images/Pic10.jpeg" alt="">
+			<img src="css/images/Pic1.jpg" alt="img">
+			<img src="css/images/Pic2.jpg" alt="">
+			<img src="css/images/Pic3.jpg" alt="">
+			<img src="css/images/Pic4.jpg" alt="">
+			<img src="css/images/Pic5.jpeg" alt="">
+			<img src="css/images/Pic6.jpg" alt="">
+			<img src="css/images/Pic7.jpg" alt="">
+			<img src="css/images/Pic8.jpg" alt="">
+			<img src="css/images/Pic9.jpg" alt="">
+			<img src="css/images/Pic10.jpeg" alt="">
 		</div>
 	</div>
 
