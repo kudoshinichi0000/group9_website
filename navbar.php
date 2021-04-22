83b0da58d6bae3ec8ad95099db2527425d1cecdd
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-23 01:46:24 +0800

    .

--- navbar.php
+++ navbar.php
@@ -8,7 +8,7 @@
               <div class="home"><a href="main.php">Home</a></div>
             </li>
             <li>
-              <div class="About"><a href"About.php">About</a></div>
+              <div class="about"><a href"About.php">About</a></div>
             </li>
             <li>
               <div class="results"><a href="results.php"><b>History</b></a></div>
