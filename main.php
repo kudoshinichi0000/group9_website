f83869ee511d836a425b5a641d1747bece5ffa06
agapito <agapitojezreel25@gmail.com>
2021-04-23 01:31:05 +0800

    sana ayos lang ako

--- main.php
+++ main.php
@@ -20,29 +20,28 @@
 	</div><br>
 
 	<div class="center">Welcome to Feedopedia</div><br>
-<!--- Lalagyan ng mga <picture> -->
-<div class="Con">
-	<div class="carousel-container">
-		<i class="fas fa-arrow-left" id="PrevBtn"></i>
-		<i class="fas fa-arrow-right" id="nextBtn"></i>
-		<div class="carousel-slide">
-			<img src="css/images/Pic10.jpeg" id="lastClone" alt="" style="width: 105%">
-			<img src="css/images/Pic1.jpg" alt="" style="width: 105%">
-			<img src="css/images/Pic2.jpg" alt="" style="width: 105%">
-			<img src="css/images/Pic3.jpg" alt="" style="width: 105%">
-			<img src="css/images/Pic4.jpg" alt="" style="width: 105%">
-			<img src="css/images/Pic5.jpeg" alt="" style="width: 107%">
-			<img src="css/images/Pic6.jpg" alt="" style="width: 105%">
-			<img src="css/images/Pic7.jpg" alt="" style="width: 107%">
-			<img src="css/images/Pic8.jpg" alt="" style="width: 107%">
-			<img src="css/images/Pic9.jpg" alt="" style="width: 107%">
-			<img src="css/images/Pic10.jpeg" alt="" style="width: 107%">
-			<img src="css/images/Pic1.jpg" id="firstClone" alt="" style="width: 105%">
+	<div class="Con">
+		<div class="carousel-container">
+			<i class="fas fa-arrow-left" id="PrevBtn"></i>
+			<i class="fas fa-arrow-right" id="nextBtn"></i>
+			<div class="carousel-slide">
+				<img src="css/images/Pic10.jpeg" id="lastClone" alt="" style="width: 105%">
+				<img src="css/images/Pic1.jpg" alt="" style="width: 105%">
+				<img src="css/images/Pic2.jpg" alt="" style="width: 105%">
+				<img src="css/images/Pic3.jpg" alt="" style="width: 105%">
+				<img src="css/images/Pic4.jpg" alt="" style="width: 105%">
+				<img src="css/images/Pic5.jpeg" alt="" style="width: 107%">
+				<img src="css/images/Pic6.jpg" alt="" style="width: 105%">
+				<img src="css/images/Pic7.jpg" alt="" style="width: 107%">
+				<img src="css/images/Pic8.jpg" alt="" style="width: 107%">
+				<img src="css/images/Pic9.jpg" alt="" style="width: 107%">
+				<img src="css/images/Pic10.jpeg" alt="" style="width: 107%">
+				<img src="css/images/Pic1.jpg" id="firstClone" alt="" style="width: 105%">
+			</div>
 		</div>
-	</div>
-</div><br>
-
+	</div><br>
 	<div class="center">News Feed</div>
+
 
 	<div class="DisplayQuestions">
 		<!---Dito i didisplay ang mga questions--->
@@ -50,7 +49,6 @@
 	</div>
 
 	<script src="app.js"></script>
-
 </body>
 	<?php include_once "footer.php";?>
 </html>
