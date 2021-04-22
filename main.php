33931ade660d7403e781b5fd496c65b34f9d7a52
agapito <agapitojezreel25@gmail.com>
2021-04-22 09:20:14 +0800

    yow

--- main.php
+++ main.php
@@ -16,7 +16,23 @@
 	<?php include_once "db.php"; include_once "navbar.php"; ?>
 
 <!--- Lalagyan ng mga <picture> -->
+	<div class="carousel-container">
+		<div class="carousel-slide">
+			<img src="images/Pic1" alt="">
+			<img src="images/Pic2" alt="">
+			<img src="images/Pic3" alt="">
+			<img src="images/Pic4" alt="">
+			<img src="images/Pic5" alt="">
+			<img src="images/Pic6" alt="">
+			<img src="images/Pic7" alt="">
+			<img src="images/Pic8" alt="">
+			<img src="images/Pic9" alt="">
+			<img src="images/Pic10" alt="">
+		</div>
+	</div>
 
+	<button id="prevBtn">Prev</button>
+	<button id="nextBtn">Next</button>
 	<!---Dito ididisplay yung mga quizes--->
 
 
