ac7a2ec13bbff0b0cd4058b7a6b3160bd18f8ff8
agapito <agapitojezreel25@gmail.com>
2021-04-22 11:33:49 +0800

    awdaw

--- app.js
+++ app.js
@@ -19,3 +19,10 @@ nextBtn.addEventListener('click', () => {
   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 });
+
+prevBtn.addEventListener('click', () => {
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
