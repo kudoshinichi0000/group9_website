f40940a2b4b8ac989fd8f84be41c859f39a00a1f
agapito <agapitojezreel25@gmail.com>
2021-04-22 12:21:10 +0800

    sefsfsfs

--- app.js
+++ app.js
@@ -29,14 +29,14 @@ prevBtn.addEventListener('click', () => {
 });
 
 carouselSlide.addEventListener('transitionend', () => {
-  if(carouselImages[counter]. id === 'lastClone'){
+  if(carouselImages[counter].id === 'lastClone') {
     carouselSlide.style.transition = "none";
-    counter = carouselImages.length - 2 ;
+    counter = carouselImages.length -  2 ;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
   }
-  if(carouselImages[counter]. id === 'firstClone'){
+  if(carouselImages[counter].id === 'firstClone') {
     carouselSlide.style.transition = "none";
     counter = carouselImages.length - counter;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
+  }
 });
