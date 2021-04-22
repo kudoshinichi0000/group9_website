840b45010b112b9a6858b5b6077b29d634119400
agapito <agapitojezreel25@gmail.com>
2021-04-22 10:45:49 +0800

    adaw

--- css/app.js
+++ css/app.js
@@ -10,3 +10,10 @@ let counter = 1;
 const size = carouselImages[0].clientwidth;
 
 carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
+
+//Button Listeners
+nextBtn.addEventListener('click' ,()=>{
+  carouselSlide.style.transition = "transform 0.4s easein-out";
+  counter++;
+  console.log(counter);
+});
