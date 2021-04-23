dda8f30eb75694420e270d4fa6631c66b4c4057d
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-23 22:01:51 +0800

    remove

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Feedopedia</title>
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<link rel="stylesheet" type="text/css" href="css/navbar.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0;">
@@ -10,48 +10,9 @@
 
 </head>
 <body>
+	<footer>
 
 </body>
 
-<!-- Footer -->
-	<section id="footer">
-		<div class="container">
-			<div class="row text-center text-xs-center text-sm-left text-md-left">
 
-				<div class="col-xs-12 col-sm-5 col-md-2">
-					<ul class="list-unstyled s-links">
-						<li><a href="main.php"><i class="fa fa-angle-double-right"></i>Home</a></li>
-					</ul>
-				</div>
-				<div class="col-xs-12 col-sm-5 col-md-2">
-					<ul class="list-unstyled s-links">
-						<li><a href="#"><i class="fa fa-angle-double-right"></i>Services</a></li>
-					</ul>
-				</div>
-
-				<div class="col-xs-12 col-sm-5 col-md-2">
-					<ul class="list-unstyled s-links">
-						<li><a href="Contact.php"><i class="fa fa-angle-double-right"></i>Contact</a></li>
-					</ul>
-				</div>
-
-				<div class="col-xs-12 col-sm-5 col-md-2">
-					<ul class="list-unstyled s-links">
-						<li><a href="#"><i class="fa fa-angle-double-right"></i>Terms</a></li>
-					</ul>
-				</div>
-
-				<div class="col-xs-12 col-sm-5 col-md-2">
-					<h5>Policy</h5>
-					<ul class="list-unstyled s-links">
-						<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i>Data Policy</a></li>
-					</ul>
-					<ul class="list-unstyled s-links">
-						<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i>Cookies Policy</a></li>
-					</ul>
-				</div></div><br>
-
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p class="h6">Copyright ©2021 GROUP9 </p>
 </html>
