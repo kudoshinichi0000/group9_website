b231e3f5425f1b2273ce7943da64072b2f2f5ca6
agapito <agapitojezreel25@gmail.com>
2021-04-23 14:11:34 +0800

    pop

---
+++ Categories.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <img src="css/images/Logo.png" alt="" height="50" style="float: left; padding-top: 0.5em;">
+        <div class="TextDesign"><h3>Feedopedia</h3></div>
+          <nav>
+            <ul>
+              <li>
+                <div class="home"><a href="main.php">Home</a></div>
+              </li>
+              <li>
+                <div class="about"><a href"About.php">About</a></div>
+              </li>
+              <li>
+                <div class="results"><a href="results.php"><b>History</b></a></div>
+              </li>
+              <li>
+                <div class="admin"><a href="login.php"><b>Admin</b></a></div>
+              </li>
+            </ul>
+      </nav>
+    </div>
+  </body>
+</html>
