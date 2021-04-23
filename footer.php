3b1a42e25b1ab44a6d1a04b2c29d201e330b6302
agapito <agapitojezreel25@gmail.com>
2021-04-23 12:01:30 +0800

    ang saket na ng ulo ko whaaaa

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
 	<title></title>
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0;">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
