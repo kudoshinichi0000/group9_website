af8653fa451d896391f9b7ca9878c144e7e6886f
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-24 16:16:26 +0800

    q

--- footerr.php
+++ footerr.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 <title> Feedopedia</title>
-<meta name="viewport" content="width="
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://macdn.bootstrapcdn.com"
 </head>
 
 <body>
