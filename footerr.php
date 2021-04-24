f3a8dd136e4feb7ba45fc9609996caae9ce2df9f
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-24 16:16:55 +0800

    q

--- footerr.php
+++ footerr.php
@@ -3,7 +3,7 @@
 <head>
 <title> Feedopedia</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://macdn.bootstrapcdn.com"
+<link rel="stylesheet" href="https://macdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head>
 
 <body>
