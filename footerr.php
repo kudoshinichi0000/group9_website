8337d78929f0aaffd7c64fd4cd344a2a57f24fe0
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-24 16:49:15 +0800

    d

--- footerr.php
+++ footerr.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+  <link rel="stylesheet" type="text/text/css" href="css/footer.css"
 </head>
 <body>
     <footer>
