7537e4887e5dbc6896642e9babe6ce8bf0440d42
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-24 16:35:41 +0800

    f

--- footerr.php
+++ footerr.php
@@ -24,6 +24,8 @@
                   </ul>
               </div>
               <div class="col-md-3 col-sm-6 col-xs-12 segment-three">
+                  <h2> Contacts</h2>
+                  <p> You can visit out social media accounts for transactions</p>
               </div>
             </div>
           </div>
