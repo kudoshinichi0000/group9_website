6ef94cd0e0113694d8cfac3e99b9505c6fa0a139
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-24 16:24:33 +0800

    d

--- footerr.php
+++ footerr.php
@@ -6,8 +6,22 @@
 <link rel="stylesheet" href="https://macdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://stckpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-
 <body>
+    <footer>
+        <div class="footer-top">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-3 col-sm-6 col-xs-12 segment-one">
+                  <h3> Feedopedia</h3>
+                  <p> Feedopedia is an entertainment and educatioal websites.....asdfdghfjdkkllakkajfkldkfldslfkiduieuwifhndvmnmvs</p>
+              </div>
+              <div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+    </footer>
 </body>
 
 </html>
