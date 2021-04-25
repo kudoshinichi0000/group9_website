b8b64562046c8d59c154daeaeac164abaf5a619a
agapito <agapitojezreel25@gmail.com>
2021-04-25 23:17:35 +0800

    antok na ko

---
+++ addminNav.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
+      position: -webkit-sticky; /* Safari */
+      position: sticky;
+      top: 0;
+}
+
+  li {
+    float: left;
+}
+
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+  li a:hover {
+    background-color: #111;
+}
+
+  .active {
+  background-color: #4CAF50;
+}
+    </style>
+  </head>
+  <body>
+    <ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+</ul>
+  </body>
+</html>
