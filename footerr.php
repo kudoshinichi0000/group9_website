333ab664c94384d921cd279bb6c2a5c3bc94ca93
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-26 01:23:30 +0800

    f

--- footerr.php
+++ footerr.php
@@ -78,7 +78,7 @@
       font-family: Poppins;
       text-transform: uppercase;
     }
-    .segment-three h2:before;{
+    .segment-four h2:before;{
       content: '|';
       color: #c65039;
       padding-right: 10px;
