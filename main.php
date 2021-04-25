651dadba3220047b7688dcc91eb9faacc83f2ee2
agapito <agapitojezreel25@gmail.com>
2021-04-25 14:55:24 +0800

    admin

--- main.php
+++ main.php
@@ -19,31 +19,9 @@
 		<!---Dito ilalagay yung mga different kinds of categories ng mga questions--->
 	</div><br>
 
-<!-- Gagawin ko nalang tong every seconds magpapalit ng pictures para wala ng i cli click
-	<div class="centerBlack">Welcome to Feedopedia</div><br>
-	<div class="Con">
-		<div class="carousel-container">
-			<i class="fas fa-arrow-left" id="PrevBtn"></i>
-			<i class="fas fa-arrow-right" id="nextBtn"></i>
-			<div class="carousel-slide">
-				<img src="css/images/Pic10.jpeg" id="lastClone" alt="" style="width: 105%">
-				<img src="css/images/Pic1.jpg" alt="" style="width: 105%">
-				<img src="css/images/Pic2.jpg" alt="" style="width: 105%">
-				<img src="css/images/Pic3.jpg" alt="" style="width: 105%">
-				<img src="css/images/Pic4.jpg" alt="" style="width: 105%">
-				<img src="css/images/Pic5.jpeg" alt="" style="width: 107%">
-				<img src="css/images/Pic6.jpg" alt="" style="width: 105%">
-				<img src="css/images/Pic7.jpg" alt="" style="width: 107%">
-				<img src="css/images/Pic8.jpg" alt="" style="width: 107%">
-				<img src="css/images/Pic9.jpg" alt="" style="width: 107%">
-				<img src="css/images/Pic10.jpeg" alt="" style="width: 107%">
-				<img src="css/images/Pic1.jpg" id="firstClone" alt="" style="width: 105%">
-			</div>
-		</div>
-	</div><br>
--->
-	<div class="centerBlack">News Feed</div>
+<!---->
 
+	<div class="centerBlack">News Feed</div>
 
 	<div class="DisplayQuestions">
 		<!---Dito i didisplay ang mga questions--->
