f357e770906805ccfdd25141f2b8db02f35528ad
agapito <agapitojezreel25@gmail.com>
2021-04-25 12:51:22 +0800

    hayst

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Main</title>
 	<link rel="stylesheet" type="text/css" href="css/navbar.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0;">
 	<!---this link is to design the font style --->
