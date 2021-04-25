24c518b5db3ef99a76359ee2267bc30ad679f1af
agapito <agapitojezreel25@gmail.com>
2021-04-25 11:51:47 +0800

    footer

--- footer.php
+++ footer.php
@@ -53,13 +53,13 @@
 				</div>
 
 				<div class="row">
-					<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					<br><div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
 						<p class="h6">Feedopedia</p>
 				</div></div>
 
 				<div class="row">
-						<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-							<br><p class="h6">Copyright ©2021 GROUP9 </p>
+						<br><div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+							<p class="h6">Copyright ©2021 GROUP9 </p>
 				</div></div>
 
 				<div class="foot">
