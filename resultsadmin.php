179497fbeecd275d1cb60f62a3481691eca249ca
agapito <agapitojezreel25@gmail.com>
2021-04-25 22:44:59 +0800

    op

--- resultsadmin.php
+++ resultsadmin.php
@@ -8,9 +8,9 @@
 	<link href="https://fonts.googleapis.com/css2?family=Orelega+One&display=swap" rel="stylesheet">
 </head>
 <body>
-	<?php include_once "db.php";//include_once "navbaradmin.php" ?>
+	<?php include_once "db.php";include_once "navbaradmin.php" ?>
 
-	<?php echo $_SESSION['userid'];?>
+	<?php //echo $_SESSION['userid'];?>
 
 </body>
 	<?php include_once "footer.php";?>
