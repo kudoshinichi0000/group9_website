298c39e4ecf0fe49bfbfa0da9d6015cc756e3c3e
agapito <agapitojezreel25@gmail.com>
2021-04-25 22:41:44 +0800

    pop

--- navbaradmin.php
+++ navbaradmin.php
@@ -1,7 +1,7 @@
 <header>
     <div class="container">
       <img src="css/images/Logo.png" alt="" height="50" style="float: left; padding-top: 0.5em; ">
-      <div class="logo"><h3>Feedopedia</h3></div>
+      <div class="TextDesign">Feedopedia</div>
       <nav>
         <ul>
           <li><div class="logout"><a href="logout.php"><b>Logout</b></a></div></li>
