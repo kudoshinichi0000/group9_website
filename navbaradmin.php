f17c84438123ac950b6fb4e60f2d975a0bc3fd13
agapito <agapitojezreel25@gmail.com>
2021-04-25 22:56:02 +0800

    taylor swift is the best

--- navbaradmin.php
+++ navbaradmin.php
@@ -6,6 +6,16 @@
         <ul>
           <li><div class="logout"><a href="logout.php"><b>Logout</b></a></div></li>
         </ul>
+        <ul>
+          <div id="dropdown">
+            <button id="dropbtn">Dropdown</button>
+              <div id="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+        </ul>
       </nav>
     </div>
 </header><br><br>
