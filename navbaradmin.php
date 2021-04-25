9d93cc52f665f6905efe760d9537b4770cea8048
agapito <agapitojezreel25@gmail.com>
2021-04-26 00:18:08 +0800

    napapaisip ako

--- navbaradmin.php
+++ navbaradmin.php
@@ -1,11 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Navbar</title>
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+  </head>
+  <body>
 <header>
-    <div class="container">
-      <img src="css/images/Logo.png" alt="" height="50" style="float: left; padding-top: 0.5em; ">
+  <div class="container">
+    <img src="css/images/Logo.png" alt="" height="50" style="float: left; padding-top: 0.5em;">
       <div class="TextDesign">Feedopedia</div>
-      <nav>
-        <ul>
-          <li><div class="logout"><a href="logout.php"><b>Logout</b></a></div></li>
-        </ul>
-      </nav>
-    </div>
+        <nav>
+          <ul>
+              <li><div class="logout"><a href="logout.php"><b>Logout</b></a></div></li>
+          </ul>
+        </nav>
+  </div>
 </header>
+</body>
+</html>
