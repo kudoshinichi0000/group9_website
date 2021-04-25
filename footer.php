e8e46cfab2fd1c37c3099c3321b956778deea772
agapito <agapitojezreel25@gmail.com>
2021-04-25 11:43:47 +0800

    pop

--- footer.php
+++ footer.php
@@ -41,20 +41,22 @@
 				</div>
 
 				<div class="col-xs-12 col-sm-5 col-md-2">
-					<h5>Policy</h5>
 					<ul class="list-unstyled s-links">
 						<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i>Data Policy</a></li>
 					</ul>
+				</div>
+
+				<div class="col-xs-12 col-sm-5 col-md-2">
 					<ul class="list-unstyled s-links">
 						<li><a href="PrivacyPolicy.php"><i class="fa fa-angle-double-right"></i>Cookies Policy</a></li>
 					</ul>
-				</div></div><br>
+				</div>
 
 				<div class="row">
 					<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
 						<p class="h6">Feedopedia</p>
 				</div></div>
-				
+
 				<div class="row">
 						<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
 							<p class="h6">Copyright ©2021 GROUP9 </p>
