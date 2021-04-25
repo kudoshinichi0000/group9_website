c62c57b1554979e51f7ca6c19c7a2705d1ec07de
agapito <agapitojezreel25@gmail.com>
2021-04-25 23:04:10 +0800

    yea

--- navbaradmin.php
+++ navbaradmin.php
@@ -36,9 +36,9 @@
 
     #dropdown-content a:hover {background-color: #ddd;}
 
-    #dropdown:hover .dropdown-content {display: block;}
+    #dropdown:hover #dropdown-content {display: block;}
 
-    #dropdown:hover .dropbtn {background-color: #3e8e41;}
+    #dropdown:hover #dropbtn {background-color: #3e8e41;}
     </style>
   </head>
   <body>
