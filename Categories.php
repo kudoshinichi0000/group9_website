1e8dd0848ed593dd6b9e6b27ef619f2727bd53c3
agapito <agapitojezreel25@gmail.com>
2021-04-25 12:26:19 +0800

    pop

--- Categories.php
+++ Categories.php
@@ -27,16 +27,23 @@
     #niv li a:hover{
       background-color: #feab02;
     }
-
     </style>
   </head>
   <body>
     <div id="niv">
       <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
+        <li><a href="#">Category 1</a></li>
+        <li><a href="#">Category 2</a></li>
+        <li><a href="#">Category 3</a></li>
+        <li><a href="#">Category 4</a></li>
+        <li><a href="#">Category 5</a></li>
+        <li><a href="#">Category 6</a></li>
+        <li><a href="#">Category 7</a></li>
+        <li><a href="#">Category 8</a></li>
+        <li><a href="#">Category 9</a></li>
+        <li><a href="#">Category 10</a></li>
+        <li><a href="#">Category 11</a></li>
+
       </ul>
     </nav>
   </body>
