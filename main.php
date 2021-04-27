f2924d503d2732e104eec59049b4c4b99c27b5e5
agapito <agapitojezreel25@gmail.com>
2021-04-27 22:26:21 +0800

    awdwadwa

--- main.php
+++ main.php
@@ -31,13 +31,13 @@
 
 					<tr>
 						<th id="cent">
-							<a href="#" style="border: 1px solid black; padding: 11px 7em;">Educational</a>
+							<a href="#" style="border: 1px solid black; padding: 11px 8em;">Educational</a>
 						</th>
 					</tr>
 
 					<tr >
 						<th id="cent">
-							<a href="#" style="border: 1px solid black; padding: 11px 7em;">Entertainment</a>
+							<a href="#" style="border: 1px solid black; padding: 11px 7.5em;">Entertainment</a>
 							</th>
 					</tr>
 
