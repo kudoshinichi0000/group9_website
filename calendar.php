ed44f3f855b0033954f64663bceb4468bb1eb85b
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-28 19:04:49 +0800

    f

--- calendar.php
+++ calendar.php
@@ -110,7 +110,7 @@ html{
     <div class="month">
       <i class="fas fa-angle-left prev"></i>
       <div class="date">
-        <h1> May</h1>
+        <h1></h1>
         <p> Sat May 29, 2021</p>
       </div>
       <i class="fas fa-angle-right next"></i>
