0b84c48c8416f13f831c41dc1a37eaba52d0b024
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-28 18:23:42 +0800

    x

--- css/script/script.js
+++ css/script/script.js
@@ -64,7 +64,7 @@ for(let i= 1; i<= lastDay; i++){
 }
 for(let k = 1; k <= nextDays; k++){
   days += '<div class="next-date">${k}</div>';
-  mpnthDays.innerHTML = days;
+  monthDays.innerHTML = days;
 }
 };
 
