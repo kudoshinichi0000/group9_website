7b6d3a780b346a53e7fa3d5bedf31b5f3e930bad
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-28 18:07:30 +0800

    f

--- css/script/script.js
+++ css/script/script.js
@@ -37,4 +37,5 @@ const months =[
 document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 document.querySelector(".date p").innerHTML =new Date().toDateString();
 let days ="";
+for(let x = firstDayIndex; x > 0;)
 }
