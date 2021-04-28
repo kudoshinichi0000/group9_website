51734090049f8fdbe87a22dc2a9a8b52a8d31d80
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-28 11:29:35 +0800

    d

--- calendar.php
+++ calendar.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <head>
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <link rel="stylesheet" href="navbar.css">
 <body>
 </body>
+<script  src="script.js"></script>
 </head>
