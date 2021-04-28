02b21e2820185db29f925a244bfa69176d78f25f
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-28 11:33:08 +0800

    link

--- calendar.php
+++ calendar.php
@@ -2,11 +2,12 @@
 <head>
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <link rel="stylesheet" href="navbar.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
 <body>
 </body>
 <div class="container">
   <div class="calendar">
-    
+
   </div>
 </div>
 <script  src="script.js"></script>
