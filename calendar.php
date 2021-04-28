73443eff1d59d3941f98b356645bb05786010cae
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-28 21:06:49 +0800

    d

--- calendar.php
+++ calendar.php
@@ -2,7 +2,7 @@
 <head>
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <style>
 *{
   margin:0;
