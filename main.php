5cd3c2fdd8b284350d2a4b6497494771e2c4ce0a
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-04-29 11:40:26 +0800

    oki

--- main.php
+++ main.php
@@ -28,7 +28,7 @@
 
 	<div id="VideoIntroCenter">
 		<video width="600" controls>
-  		<source src="css/Video/IntroVideo.mp4" type="video/mp4">
+  		<source src="res/Video/IntroVideo.mp4" type="video/mp4">
 		</video>
 	</div>
 
