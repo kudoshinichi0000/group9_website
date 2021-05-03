d312cd93f47e499de79224b767be730ea5661abb
kudoshinichi0000 <princess.raynaud0808@gmail.com>
2021-05-03 19:10:42 +0800

    tey

--- footerr.php
+++ footerr.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <style media="screen">
     footer{
-      background-color: #OO221C;
+      background-color: red;
       width: 100%;
       background-color: red;
       color: white;
