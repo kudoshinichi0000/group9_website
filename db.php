0500c6aa0b8be2297b33cf501e29b27058c1d3bc
agapito <agapitojezreel25@gmail.com>
2021-05-11 01:56:09 +0800

    yo

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-	/*$dbhost = "localhost";
+	$dbhost = "localhost";
  	$dbusername = "root";
   	$dbpassword = "";
   	$dbname = "quizdb";
@@ -7,7 +7,4 @@
   	$con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
 
   	session_start();
-		*/
-
-		$conn= new mysqli('localhost','root','','quizdb')or die("Could not connect to mysql".mysqli_error($con));
 ?>
