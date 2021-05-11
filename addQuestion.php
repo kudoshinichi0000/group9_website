2bff3bfe2a5a0915164533b6389a0a3c2a69f484
agapito <agapitojezreel25@gmail.com>
2021-05-11 18:10:22 +0800

    Ayoko nang mag aral :>

--- addQuestion.php
+++ addQuestion.php
@@ -7,7 +7,7 @@
   <body>
     <div class="container1">
 
-    <form action="quiz_title.php" method="POST">
+    <form action="addQuestion.php" method="POST">
       <table border="1" height="350px" width="40%">
         <tr>
           <th colspan="2"><h2>New Question</h2>
@@ -28,7 +28,7 @@
         </tr>
 
         <tr>
-          <th colspan="2"><input type="text" name="points" required></th>
+          <th colspan="2"><input type="number" name="points" required></th>
         </tr>
 
         <!---options--->
@@ -37,41 +37,71 @@
         </tr>
 
         <tr>
-          <th colspan="2"><input type="text" name="option1" required></th>
-          <tr>
-            <th colspan="2"> <input type="radio" name="option1" value="">Question Anser</th>
-          </tr>
+          <th colspan="2">
+            <label for="A">A.</label>
+            <input type="text" name="A" required><br>
+
+            <label for="B">B.</label>
+            <input type="text" name="B" required><br>
+
+            <label for="C">C.</label>
+            <input type="text" name="C" required><br>
+
+            <label for="D">D.</label>
+            <input type="text" name="D" required><br>
+
+          </th>
+        </tr>
+
+        <!---Answer--->
+        <tr>
+          <th colspan="2"><label><h3>Answer: </h3></label></th>
         </tr>
 
         <tr>
-          <th colspan="2"><input type="text" name="option2" required></th>
-          <tr>
-            <th colspan="2"> <input type="radio" name="option2" value="">Question Anser</th>
-          </tr>
-        </tr>
-
-        <tr>
-          <th colspan="2"><input type="text" name="option3" required></th>
-          <tr>
-            <th colspan="2"> <input type="radio" name="option3" value="">Question Anser</th>
-          </tr>
-        </tr>
-
-        <tr>
-          <th colspan="2"><input type="text" name="option4" required></th>
-          <tr>
-            <th colspan="2"> <input type="radio" name="option4" value="">Question Anser</th>
-          </tr>
-
+          <th colspan="2"><input type="text" name="ans" maxlength="1" onkeypress="return /[a-d]/i.test(event.key)" oninput="this.value = this.value.toUpperCase()" required></th>
         </tr>
 
         <!--Submit--->
         <tr>
-          <th><input type="submit" name="submit" class="btn" placeholder="Save" ></th>
-          <th colspan="2"><a href="#">Cancel</a></th>
+          <th><a href="#">Cancel</a></th>
+          <th colspan="2"><input type="submit" name="submit" class="btn" placeholder="Save" ></th>
         </tr>
       </table>
     </form>
   </div>
+
+  <?php
+
+  if($_SERVER['REQUEST_METHOD'] == "POST"){
+ 		//var_dump()
+    include("db.php");
+    include("functions.php");
+    $userId = $_SESSION['userid'];
+
+    //This is for user id
+	  $query = " SELECT * FROM quiz_list WHERE admin_id = '$userId'";
+   	$execQuery = mysqli_query($con, $query);
+ 	  $fetch = mysqli_fetch_assoc($execQuery);
+    $code = $fetch['quiz_code'];
+
+    echo "$code";
+    //Vardump();
+ 		$question = $_POST["question"];
+ 		$points = $_POST["points"];
+    $answer = $_POST["ans"];
+    $optA = $_POST["A"];
+    $optB = $_POST["B"];
+    $optC = $_POST["C"];
+    $optD = $_POST["D"];
+
+ 		$insertQuestion = "INSERT INTO questions(quiz_code, question, questionPoints, answer, option1, option2, option3, option4) VALUES('$code', '$question', '$points', '$answer', '$optA', '$optB', '$optC', '$optD' )";
+ 		$execInsert = mysqli_query($con, $insertQuestion);
+    if($execInsert){
+      header("location: questions.php");
+    }
+ 	}
+
+   ?>
   </body>
 </html>
--- questions.php
+++ questions.php
@@ -7,7 +7,7 @@
 .Containerhead{
   width: 100%;
   border: 1px solid black;
-  height: 10em;
+  height: 15em;
   background-color: yellow;
   color: black;
 }
@@ -17,22 +17,21 @@
     <?php
     include("db.php");
     include("functions.php");
-
+    $code = rand();
     $userId = $_SESSION['userid'];
 
-    //This is for title
-	  $query = " SELECT *  FROM quiz_list WHERE admin_id = '$userId'";
     //This is for user id
 	  $query = " SELECT * FROM quiz_list WHERE admin_id = '$userId'";
    	$execQuery = mysqli_query($con, $query);
  	  $fetch = mysqli_fetch_assoc($execQuery);
-
+    $code = $fetch['quiz_code'];
     $title = $fetch['title'];
       //This section will provide information about the quiz
       echo "
       <div class='Containerhead'>
       <h2>title: $title</h2>
         <h2>admin id:  $userId </h2>
+        <h2>Quiz Code: $code </h2>
         <h2>Status: pending</h2>
       </div>";
 
