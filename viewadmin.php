62a4aa23dfc69e3816f897381af36cac0fe361c6
aaron <arriolaaa123@gmail.com>
2021-05-13 14:31:32 +0800

    Needs css

--- viewadmin.php
+++ viewadmin.php
@@ -1,10 +1,41 @@
+<?php include_once("db.php");
+
+$query = "SELECT * FROM admin";
+$result = mysqli_query($con, $query);
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>ViewAdmin</title>
   </head>
   <body>
-      yooooooooooooooooooo ugh
+    <h1>Admins: </h1>
+    <table align="center">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            while($admin = mysqli_fetch_assoc($result))
+                            {
+                                echo "
+                                <tr>
+                                    <td>
+                                        {$admin['username']}
+                                    </td>
+
+                                    <td>
+                                        <a class='link' href='editadmin.php?id={$admin['id']}'>Edit</a> | <a class='link' href='deladminquestion.php?id={$admin['id']}'>Delete</a>
+                                    </td>
+                                </tr>
+                                ";
+                            }
+                          ?>
+                    </tbody>
+                </table>
   </body>
 </html>
