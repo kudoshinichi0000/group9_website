1b3ffbdaedd3ba38de899ee6d47f52bc91bcbccd
agapito <agapitojezreel25@gmail.com>
2021-05-14 23:44:45 +0800

    edit multiple question

--- MultiQEditHandler.php
+++ MultiQEditHandler.php
@@ -16,10 +16,8 @@
   //Hidden Input
   $quizId = $_POST["quizId"];
 
-    $queryyy = "UPDATE multiple_questions
-    SET question = '$question', questionPoints = '$points', answer = '$answer', option1 = '$optA', option2 = '$optB', option3 = '$optC', option4 = '$optD', typeOfQuiz = '$typeOfQuiz')
-    WHERE id = '$quizId'";
-    $execQueryyy = mysqli_query($con, $queryyy);
+    $query = "UPDATE multiple_questions SET question = '$question', questionPoints = '$points', answer = '$answer', option1 = '$optA', option2 = '$optB', option3 = '$optC', option4 = '$optD', typeOfQuiz = '$typeOfQuiz' WHERE id = '$quizId'";
+    $execQueryyy = mysqli_query($con, $query);
       if($execQueryyy){
         header("location: questions.php?quiz_code=$quizC");
       }else{
