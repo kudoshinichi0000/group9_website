45fccaeb4b8a6c6069aac4da1cdfc50ec4fe73fb
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-19 00:47:20 +0800

    pass

--- editadmin.php
+++ editadmin.php
@@ -51,18 +51,19 @@
                       <div class="row form-group">
                         <div class="col">
                           <label for="password">Password :</label>
-                          <input type="password" class="form-control" placeholder="Enter your new password" name="password"
+                          <input type="password" class="form-control" placeholder="Enter your new password" name="password" required>
                         </div>
-                    </div>
+                      </div>
+                      <div class="row form-group">
+                        <div class="col">
+                        </div>
+                      </div>
+
                   </div>
                 </div>
               </div>
             </div>
-                    <label>Password: </label>
-                    <input type="password" name="password" required>
-                </div>
-                <br>
-                <div>
+
                     <label>Confirm Password: </label>
                     <input type="password" name="confirmpassword" required>
                 </div>
