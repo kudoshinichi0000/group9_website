90e69bb6b82c29edb586d404009bfa32ec5c9b43
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-18 22:33:32 +0800

    del

--- viewadmin.php
+++ viewadmin.php
@@ -45,7 +45,8 @@ $result = mysqli_query($con, $query);
                   </td>
 
                   <td>
-                    <a class='link' href='editadmin.php?id={$admin['id']}' type='button' class='btn btn-warning btn-sm'>Edit</a> | <a class='link' href='DeleteAdmin.php?id={$admin['id']}'>Delete</a>
+                    <a class='link' href='editadmin.php?id={$admin['id']}' type='button' class='btn btn-warning btn-sm'>Edit</a>
+                    ${delete_btn}
                   </td>
                 </tr>
                 ";
