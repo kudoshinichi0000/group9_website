d9c2bb4704939ef08e6e4fd41dad31489be60320
aaron <arriolaaa123@gmail.com>
2021-05-19 02:14:27 +0800

    mod create

--- add-admin.php
+++ add-admin.php
@@ -7,7 +7,7 @@
   	$userid = rand();
 
     $pass = password_hash($password, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO moderator (userid, username, password) VALUES ( '$userid', '$name', '$password')";
+    $sql = "INSERT INTO admin (userid, username, password) VALUES ( '$userid', '$name', '$password')";
     if (mysqli_query($con, $sql)) {
       echo "Moderator Created";
     } else {
