9b167491440a240542b7ad5869c86b39945a209a
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-23 21:07:29 +0800

    margin

--- viewadminuser.php
+++ viewadminuser.php
@@ -67,7 +67,7 @@ $fetchid = $admin['userid'];
                             <th scope='row'>  $fetchname</th>
                           <td>
                             <a href='DeleteAdmin.php?id={$fetchid}' type='button' class='btn btn-danger badge-pill text-right float-right' style='width:80px; text-align:center; margin:5px;'>DELETE</a>
-                            <a href='editadmin.php?id={$fetchid}' type='button' class='btn btn-info badge-pill text-centered float-right' style='width:80px; text-align: center;'>EDIT</a>
+                            <a href='editadmin.php?id={$fetchid}' type='button' class='btn btn-info badge-pill text-centered float-right' style='width:80px; text-align: center; margin:5px;'>EDIT</a>
 
                           </td>
                           </tr>
