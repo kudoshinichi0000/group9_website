9ad8b6ad9ead155fc30a7a83ae79ca875608987e
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-24 03:42:58 +0800

    i remove the extra button

--- viewadminuser.php
+++ viewadminuser.php
@@ -68,11 +68,6 @@ $fetchid = $admin['userid'];
                           <td>
                             <button type='button' data-toggle='modal' data-target='#DeleteModal'class='btn btn-danger badge-pill text-right float-right' style='width:80px; text-align:center; margin:5px;'>DELETE</button>
                             <a href='editadmin.php?id={$fetchid}' type='button' class='btn btn-info badge-pill text-centered float-right' style='width:80px; text-align: center; margin:5px;'>EDIT</a>
-                            <div class='row'>
-                              <div class='col'>
-                                <button type='button'class='btn btn-primary' data-toggle='modal' data-target='#DeleteModal'> DELETEEEE</button>
-                              </div>
-                             </div
                           </td>
                           </tr>
                     "
