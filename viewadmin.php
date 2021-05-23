418bb29d1d3f73490462649b566c6ccfe21dc83f
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-23 22:43:15 +0800

    d

--- viewadmin.php
+++ viewadmin.php
@@ -56,7 +56,7 @@ $result = mysqli_query($con, $query);
                     echo "
                       <tr>
                         <th scope='row'>
-                        {$admin['username']}<.th>
+                        {$admin['username']}</th>
                         <td>
                         <a href='editadmin.pjp?id={$admin['userid']}' Type='button' class='btn btn-danger badge-pill text-right float-right' style='width:80px; text-align:center; margin:5px;'> DELETE</a>
                         </td>
