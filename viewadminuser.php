51daa613383057e7d79bff628620ffe43e2eacb3
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-23 20:01:49 +0800

    d

--- viewadminuser.php
+++ viewadminuser.php
@@ -46,16 +46,18 @@ $fetchid = $admin['userid'];
      <?php unset($_SESSION['delsuccess']);
     endif;?>
     <div class="container">
-      <div class="card">
-        <div class="card-header">
-          FEATURED
-        </div>
-        <div class="card-body">
-          <h5 class="card-title" SPECIAL MO TO JOKE </h5>
-          <p class="card-text"> With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-       </div>
+      <div class="jumbotron"
+        <div class="card">
+          <div class="card-header">
+            FEATURED
+          </div>
+          <div class="card-body">
+            <h5 class="card-title" SPECIAL MO TO JOKE </h5>
+            <p class="card-text"> With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+         </div>
+      </div>
     </div>
 
     <div class="container">
