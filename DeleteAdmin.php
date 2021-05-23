6aacaeb8951a86adf13c1043af896b48300acc89
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-24 00:42:20 +0800

    f

--- DeleteAdmin.php
+++ DeleteAdmin.php
@@ -38,7 +38,7 @@
               </form>
             </div>
             <div class="modal-footer">
-              <input type="button"name="choice" class="btn btn-secondary" value="yes">YESido</button>
+              <input type="submit"name="choice" class="btn btn-secondary" value="yes">YESido</button>
                 <button type="submit" name="choice" class="btn btn-danger" value="no">NO</button>
                 <!--<button type="submit" class="btn btn-secondary" data-dismiss="modal">YES</button>!-->
                   <input type="hidden" name="id" value=<?php echo $adminid ?>>
