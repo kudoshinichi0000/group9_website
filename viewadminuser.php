e4f85a482a1b4f0b84d2b6001c6ffa220565a6dc
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-24 03:04:03 +0800

    taenang yan

--- viewadminuser.php
+++ viewadminuser.php
@@ -68,6 +68,11 @@ $fetchid = $admin['userid'];
                           <td>
                             <button type='button' class='btn btn-danger badge-pill text-right float-right'  data-toggle='modal' data-target='#DeleteModal' style='width:80px; text-align:center; margin:5px;'>DELETE</button>
                             <a href='editadmin.php?id={$fetchid}' type='button' class='btn btn-info badge-pill text-centered float-right' style='width:80px; text-align: center; margin:5px;'>EDIT</a>
+                            <div class='row'>
+                              <div class='col'>
+                                <button type='button' class='btn btn-primary' data-toggle='modal' data-target=''#DeleteModal'> DELETEEEE</button>
+                              </div>
+                             </div
                           </td>
                           </tr>
                     "
