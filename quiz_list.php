6a954ba836cfdee620cdfaad36d9759e3f154c97
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-25 23:55:15 +0800

    try

--- quiz_list.php
+++ quiz_list.php
@@ -121,7 +121,7 @@
             </div>
             <div class="modal-body">
               <form action="DelQuizHandler.php" id="form-delete-user" method="post">
-                  <label><p>  Are you sure you want to delete your account: <?php echo $code?>?</p></label>
+                  <label><p>  Are you sure you want to delete this quiz: <?php echo $Ftitle  $code?>?</p></label>
 
             </div>
             <div class="modal-footer">
