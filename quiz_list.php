2da841147ce734fc130ec56c4bad4aabd765672a
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-25 22:07:34 +0800

    table

--- quiz_list.php
+++ quiz_list.php
@@ -9,6 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
     <style media="screen">
     table {
       background-color: #fff;
@@ -39,6 +40,9 @@
         <div class="card">
           <div class="card-header">
             <h2><b>QUIZ DETAILS<b></h2>
+          </div>
+            <div class="card-body">
+            <table class="tabl table-striped table-hover table-bordered"
     <a href='quiz_title.php' class='addquiz'>Add Quiz</a><br><br>
       <table height='50px' width='85%'>
         <tr>
