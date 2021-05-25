7b54c0387b822f1cd90cbbdf8639157b0cf361ab
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-25 22:12:14 +0800

    try

--- quiz_list.php
+++ quiz_list.php
@@ -44,63 +44,63 @@
           </div>
             <div class="card-body">
             <table class="tabl table-striped table-hover table-bordered"
-    <a href='quiz_title.php' class='addquiz'>Add Quiz</a><br><br>
-      <table height='50px' width='85%'>
-        <tr>
-          <th colspan='2'><h2><label>Title: </label></h2>
-          <th colspan='2'><h2><label>Items: </label></h2>
-          <th colspan='2'><h2><label>Total Points: </label></h2>
-          <th colspan='2'><h2><label>Categories: </label></h2>
-          <th colspan='2'><h2><label>Quiz Code: </label></h2>
-          <th colspan='3'><h2><label>Action: </label></h2>
-        </tr>
-        <br>
+              <a href='quiz_title.php' class='addquiz'>Add Quiz</a><br><br>
+                <thead>
+                  <tr>
+                    <th scope="col"><h2><label>Title: </label></h2></th>
+                    <th colspan='2'><h2><label>Items: </label></h2>
+                    <th colspan='2'><h2><label>Total Points: </label></h2>
+                    <th colspan='2'><h2><label>Categories: </label></h2>
+                    <th colspan='2'><h2><label>Quiz Code: </label></h2>
+                    <th colspan='3'><h2><label>Action: </label></h2>
+                  </tr>
+                  <br>
 
-    <?php
-    //Step 1 Database Connectivity
-    include_once "db.php";
-    $userid = $_SESSION['userid'];
+              <?php
+              //Step 1 Database Connectivity
+              include_once "db.php";
+              $userid = $_SESSION['userid'];
 
-    $query = "SELECT * FROM quiz_list";
-    $execQuery = mysqli_query($con, $query);
-    while ($fetchId = mysqli_fetch_assoc($execQuery)) {
-    $admin_id = $fetchId["admin_id"];
+              $query = "SELECT * FROM quiz_list";
+              $execQuery = mysqli_query($con, $query);
+              while ($fetchId = mysqli_fetch_assoc($execQuery)) {
+              $admin_id = $fetchId["admin_id"];
 
-    if ($userid == $admin_id) {
-      $query = "SELECT * FROM quiz_list WHERE admin_id = $admin_id ";
-      $execQuery = mysqli_query($con, $query);
-      while ($fetchTitle = mysqli_fetch_assoc($execQuery)) {
-      $title = $fetchTitle["title"];
-      $code = $fetchTitle['quiz_code'];
-      $items = $fetchTitle['items'];
-      $OS = $fetchTitle['OverallScores'];
-      $Cat= $fetchTitle['categories'];
+              if ($userid == $admin_id) {
+                $query = "SELECT * FROM quiz_list WHERE admin_id = $admin_id ";
+                $execQuery = mysqli_query($con, $query);
+                while ($fetchTitle = mysqli_fetch_assoc($execQuery)) {
+                $title = $fetchTitle["title"];
+                $code = $fetchTitle['quiz_code'];
+                $items = $fetchTitle['items'];
+                $OS = $fetchTitle['OverallScores'];
+                $Cat= $fetchTitle['categories'];
 
-      if(strlen($title) >= 1){
-            $Ftitle = substr($title,0,20) . "...";
+                if(strlen($title) >= 1){
+                      $Ftitle = substr($title,0,20) . "...";
 
-            echo "
-              <tr>
-                <br><br>
-                <th colspan='2'><h2>$Ftitle</h2></th>
-                <th colspan='2'><h2>$items</h2></th>
-                <th colspan='2'><h2>$OS</h2></th>
-                <th colspan='2'><h2>$Cat</h2></th>
-                <th colspan='2'><h2>$code</h2></th>
+                      echo "
+                        <tr>
+                          <br><br>
+                          <th colspan='2'><h2>$Ftitle</h2></th>
+                          <th colspan='2'><h2>$items</h2></th>
+                          <th colspan='2'><h2>$OS</h2></th>
+                          <th colspan='2'><h2>$Cat</h2></th>
+                          <th colspan='2'><h2>$code</h2></th>
 
-                <th colspan='3'>
-                  <a href='editTitle.php?quiz_code=$code' class='action'>edit title</a>
-                  <a href='questions.php?quiz_code=$code' class='action'>edit/add questions</a>
-                  <a href='deleteQuiz.php?quiz_code=$code' class='action'>delete</a>
-                </th>
-              </tr>
+                          <th colspan='3'>
+                            <a href='editTitle.php?quiz_code=$code' class='action'>edit title</a>
+                            <a href='questions.php?quiz_code=$code' class='action'>edit/add questions</a>
+                            <a href='deleteQuiz.php?quiz_code=$code' class='action'>delete</a>
+                          </th>
+                        </tr>
 
-              ";
-            }
-          }
-        }
-      }
-     ?>
+                        ";
+                      }
+                    }
+                  }
+                }
+               ?>
             </div>
           </div>
         </div>
