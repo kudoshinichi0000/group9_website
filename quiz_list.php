0058c88d1f19c1ef16a5f6946e48c4152d97eb2c
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-25 22:01:45 +0800

    csslink

--- quiz_list.php
+++ quiz_list.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <title>Quiz list</title>
     <link rel="stylesheet" type="text/css" href="css/createQuiz.css">
+    <script src="js/bootstrap.bundle.min.js"> </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <style media="screen">
     table {
       background-color: #fff;
