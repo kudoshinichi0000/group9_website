54397296534614c3ae523ec676f35c842b53d6b8
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-26 01:23:23 +0800

    h

--- quiz_list.php
+++ quiz_list.php
@@ -44,7 +44,7 @@
             <table class="table table-striped table-hover table-bordered">
                 <thead>
                   <tr>
-                    <th scope="col"><h4>Title:</h4></th>
+                    <th scope="col"><h4><label>Title: </label></h4></th>
                     <th scope="col"><h4><label>Items: </label></h4></th>
                     <th scope="col"><h4><label>Total Points: </label></h4></th>
                     <th scope="col"><h4><label>Categories: </label></h4></th>
