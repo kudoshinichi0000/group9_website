fd61060a175ac609c7c5677b7f0b7b5b8549e882
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-27 23:06:00 +0800

    t

--- Identification.php
+++ Identification.php
@@ -20,35 +20,37 @@
       <br><br><br><br>
      <div class="container">
        <div class="jumbotron">
-         <div class="card-header">
-           <h2><b> Adding Identification Question<b></h2>
-         </div>
-         <div class="card-body">
-           <form action='IdentificationHandler.php' method='POST'>
-             <table border='1' height='350px' width='25%' class='container1'>
-
-               <tr>
-                 <th colspan='3'><h2><label for="IdenQuestion">Question: </label> </h2>
-                 <h2><input type="text" name="IdenQuestion" required> </h2></th>
-               </tr>
-
-               <tr>
-                 <th colspan='3'><h2><label for="IdenAnswer">Answer: </label> </h2>
-                   <input type="text" name="IdenAnswer" required></th>
-               </tr>
+         <div class="card">
+           <div class="card-header">
+             <h2><b> Adding Identification Question<b></h2>
+           </div>
+           <div class="card-body">
+             <form action='IdentificationHandler.php' method='POST'>
+               <table border='1' height='350px' width='25%' class='container1'>
 
                  <tr>
-                   <th colspan='3'><h2><label for="points">points: </label> </h2>
-                   <h2><input type="number" name="points" required> </h2></th>
-               </tr>
-               <tr>
-                 <?php echo "<th colspan='2'><a href='questions.php?quiz_code=$code'>Cancel</a></th>"; ?>
-                 <th colspan='3'><input type='submit' name='submit' class='btn' placeholder='Save' ></th>
-               </tr>
-             </table>
-             <input type='hidden' name='quizCode' value='<?php echo $code ?>'>
-           </form>
+                   <th colspan='3'><h2><label for="IdenQuestion">Question: </label> </h2>
+                   <h2><input type="text" name="IdenQuestion" required> </h2></th>
+                 </tr>
 
+                 <tr>
+                   <th colspan='3'><h2><label for="IdenAnswer">Answer: </label> </h2>
+                     <input type="text" name="IdenAnswer" required></th>
+                 </tr>
+
+                   <tr>
+                     <th colspan='3'><h2><label for="points">points: </label> </h2>
+                     <h2><input type="number" name="points" required> </h2></th>
+                 </tr>
+                 <tr>
+                   <?php echo "<th colspan='2'><a href='questions.php?quiz_code=$code'>Cancel</a></th>"; ?>
+                   <th colspan='3'><input type='submit' name='submit' class='btn' placeholder='Save' ></th>
+                 </tr>
+               </table>
+               <input type='hidden' name='quizCode' value='<?php echo $code ?>'>
+             </form>
+
+           </div>
          </div>
        </div>
      </div>
