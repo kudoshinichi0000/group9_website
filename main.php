5d6c63e8480ff6a841a9e51d92b9b0c20af77118
agapito <agapitojezreel25@gmail.com>
2021-05-28 03:08:31 +0800

    kapeng matamang

--- main.php
+++ main.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 	<title>Main</title>
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0;">
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Orelega+One&display=swap" rel="stylesheet">
-	<link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" crossorigin="anonymous">
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/styles.css">
+				<link type="text/css" rel="stylesheet" href="css/navbar.css">
+        <script src="https://use.fontawesome.com/52b2061ad6.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<style media="screen">
 		.wel{
 			text-align: center;
@@ -19,25 +20,49 @@
 			width: 80%;
 			border-radius: 5em;
 		}
+
+		#Maincontainer{
+			border: 1px solid black;
+			width: 80%;
+			margin: auto;
+		}
+		.DisplayQuestions{
+			margin-left: 2em;
+			border: 1px solid black;
+			width: 50%;
+			height: 11em;
+			color: black;
+		}
 	</style>
 </head>
 <body>
 	<?php
 		include_once("navbar.php");
-	?>
-	<br><br><br><br><br><br>
-		<div class="wel">
-			Hello, Welcome To Feedopedia!
-		</div>
+	?><br><br><br><br><br><br>
+
 	<br><br><br>
 		<div id="VideoIntroCenter">
 			<video width="600" controls>
 	  		<source src="res/Video/IntroVideo.mp4" type="video/mp4">
 			</video>
 		</div>
-	</div>
+	</div><br><br><br>
 
-	<!--displaying all Quiz--->
+		<div id="Maincontainer">
+
+			<b style="font-size: 4em;">BuzzFeed Quizzes</b>
+			<p>We've got all the quizzes you love to binge! Come on in and hunker down for the long haul.</p>
+
+			<div class="Categories">
+				<p style="text-align: center; align-items: center;">Our Categories</p><br>
+				<a href="#">Educational</a>
+				<a href="#">Entertainment</a>
+				<a href="#">Mix</a>
+			</div>
+
+
+
+			<!--displaying all Quiz--->
 	<div class="centerBlack">News Feed</div>
 			<?php
 				include_once("db.php");
@@ -55,23 +80,35 @@
 					if(strlen($title) >= 1){
 		            $Ftitle = substr($title,0,25) . "...";
 					echo "
-					<div class='DisplayQuestions'>
-						<a href='takeQuizMultipleChoice.php?quiz_code=$QuizCode'
-							style='
-									color: black;
-									text-decoration: none;
-									'>
-								<img src='res/quizPicture/$pic' width='250px' height='150px' style='float: left; margin-right: 1em;' alt='image not found' >
-								<b style='font-size:2em;'>$Ftitle</b>
-								<h5>Description: $Desc</h5>
-								<h5>Categories: $Cat</h5>
-								<h5>Publication Date: $newDate</h5><br><br>
-						</a>
-					</div><br>
+								<div style='display: inline-block'>
+						      <div class='row' >
+						        <div class='col s12 m4 l4'>
+						          <div class='card'>
+						            <div class='card-image waves-effect waves-block waves-light'>
+						               <img class='activator' src='res/quizPicture/$pic' width='80px' height='150px' style=''>
+						            </div>
+						            <div class='card-content'>
+						               <span class='card-title activator grey-text text-darken-4'>$title<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
+						               <p><a href='#'>Website Link</a></p>
+						            </div>
+						            <div class='card-reveal'>
+						                <span class='card-title grey-text text-darken-4'>$title<i class='mdi-navigation-close right material-icons'>close</i></span>
+						                <p>$Desc</p>
+						            </div>
+						          </div>
+						        </div>
+						      </div>
+								</div>
+								
 					";
 				}
 			}
-			 ?>
+			?>
+
+		</div>
+
+        <script type="text/javascript" src="css/script/jquery-3.1.1.min.js"></script>
+        <script type="text/javascript" src="css/script/materialize.min.js"></script>
 
 </body>
 	<?php include_once "footerr.php";?>
