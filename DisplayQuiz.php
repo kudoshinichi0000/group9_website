b0f8ff3e5b06a8a801b0ddf82050efee8070d0b9
agapito <agapitojezreel25@gmail.com>
2021-05-28 12:36:21 +0800

    iyakkk

--- DisplayQuiz.php
+++ DisplayQuiz.php
@@ -3,13 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="css/script/materialize.min.css">
-
-    <script src="https://use.fontawesome.com/52b2061ad6.js"></script>
   </head>
   <body>
 
+    <div class='row'>
+          <div class='section scrollspy'>
+              <div class='container'>
+                  <div class='row'>
     <?php
     include_once("db.php");
     $query = "SELECT * FROM quiz_list";
@@ -23,75 +23,90 @@
       $Pub = $fetchQuiz["publish"];
       $newDate = date("m-d-Y", strtotime($Pub));
 
+
       if(strlen($title) >= 1){
             $Ftitle = substr($title,0,25) . "...";
 
+            if ($Cat == "Educational") {
             echo "
-            <div class='row'>
-                  <div class='section scrollspy'>
-                      <div class='container'>
-                          <div class='row'>
-                              <div class='col s12 m4 l4'>
-                                  <div class='card'>
-                                      <div class='card-image waves-effect waves-block waves-light'>
-                                          <img class='activator' src='res/quizPicture/$pic' width='250px' height='150px'>
-                                      </div>
-                                      <div class='card-content'>
-                                          <span class='card-title activator grey-text text-darken-4'>$title<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
-                                          <p><a href=''>Website Link</a></p>
-                                      </div>
-                                      <div class='card-reveal'>
-                                          <span class='card-title grey-text text-darken-4'>My news feed<i class='mdi-navigation-close right material-icons'>close</i></span>
-                                          <p>A web app that displays up-to-date news headlines from popular media outlets</p>
-                                      </div>
-                                  </div>
-                              </div>
+                <div class='col s12 m4 l4'>
+                  <div class='card'>
+                    <div class='card-image waves-effect waves-block waves-light'>
+                      <img class='activator' src='res/quizPicture/$pic' height='200px'>
+                    </div>
 
-                              <div class='col s12 m4 l4'>
-                                  <div class='card'>
-                                      <div class='card-image waves-effect waves-block waves-light'>
-                                          <img class='activator' src='images/newsfeed.png'>
-                                      </div>
-                                      <div class='card-content'>
-                                          <span class='card-title activator grey-text text-darken-4'>Lorem<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
-                                          <p><a href=''>Website Link</a></p>
-                                      </div>
-                                      <div class='card-reveal'>
-                                          <span class='card-title grey-text text-darken-4'>My news feed<i class='mdi-navigation-close right material-icons'>close</i></span>
-                                          <p>A web app that displays up-to-date news headlines from popular media outlets</p>
-                                      </div>
-                                  </div>
-                              </div>
+                    <div class='card-content'>
+                      <span class='card-title activator grey-text text-darken-4'>$title<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
+                      <p><a href='#'>Play quiz</a></p>
+                    </div>
 
-                              <div class='col s12 m4 l4'>
-                                  <div class='card'>
-                                      <div class='card-image waves-effect waves-block waves-light'>
-                                          <img class='activator' src='images/newsfeed.png'>
-                                      </div>
-                                      <div class='card-content'>
-                                          <span class='card-title activator grey-text text-darken-4'>Lorem<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
-                                          <p><a href=''>Website Link</a></p>
-                                      </div>
-                                      <div class='card-reveal'>
-                                          <span class='card-title grey-text text-darken-4'>My news feed<i class='mdi-navigation-close right material-icons'>close</i></span>
-                                          <p>A web app that displays up-to-date news headlines from popular media outlets</p>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
+                    <div class='card-reveal'>
+                      <span class='card-title grey-text text-darken-4'>$title<i class='mdi-navigation-close right material-icons'>close</i></span>
+                        <p>Description: $Desc</p>
+                        <p>Date: $newDate</p>
+                        <p>Category: $Cat</p>
+                    </div>
                   </div>
-              </div>
-
-
-
+                </div>
             ";
-          }
+            }
+
+        if ($Cat == "Entertainment") {
+
+        echo "
+        <div class='col s12 m4 l4'>
+            <div class='card'>
+                <div class='card-image waves-effect waves-block waves-light'>
+                    <img class='activator' src='res/quizPicture/$pic' height='200px'>
+                </div>
+                <div class='card-content'>
+                    <span class='card-title activator grey-text text-darken-4'>$title<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
+                    <p><a href='#'>Play quiz</a></p>
+                </div>
+                <div class='card-reveal'>
+                    <span class='card-title grey-text text-darken-4'>$title<i class='mdi-navigation-close right material-icons'>close</i></span>
+                    <p>Description: $Desc</p>
+                    <p>Date: $newDate</p>
+                    <p>Category: $Cat</p>
+                </div>
+            </div>
+        </div>
+
+        ";
         }
 
+        if ($Cat == "Mix") {
+        echo "
+        <div class='col s12 m4 l4'>
+          <div class='card'>
+            <div class='card-image waves-effect waves-block waves-light'>
+              <img class='activator' src='res/quizPicture/$pic' height='200px'>
+            </div>
+
+            <div class='card-content'>
+              <span class='card-title activator grey-text text-darken-4'>$title<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
+                <p><a href='#'>Play Quiz</a></p>
+            </div>
+
+            <div class='card-reveal'>
+              <span class='card-title grey-text text-darken-4'>$title<i class='mdi-navigation-close right material-icons'>close</i></span>
+              <p>Description: $Desc</p>
+              <p>Date: $newDate</p>
+              <p>Category: $Cat</p>
+            </div>
+          </div>
+      </div>
+
+        ";
+      }
+      }
+    }
      ?>
 
-
+   </div>
+</div>
+</div>
+</div>
   <script type="text/javascript" src="css/script/jquery-3.1.1.min.js"></script>
   <script type="text/javascript" src="css/script/materialize.min.js"></script>
   </body>
--- main.php
+++ main.php
@@ -7,6 +7,10 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Orelega+One&display=swap" rel="stylesheet">
 	<link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" crossorigin="anonymous">
+
+	<link href='http://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
+	<link type='text/css' rel='stylesheet' href='materialize.min.css'>
+	<script src='https://use.fontawesome.com/52b2061ad6.js'></script>
 	<style media="screen">
 		.wel{
 			text-align: center;
@@ -39,13 +43,7 @@
 		include_once("navbar.php");
 	?><br><br><br><br><br><br>
 
-	<br><br><br>
-		<div id="VideoIntroCenter">
-			<video width="600" controls>
-	  		<source src="res/Video/IntroVideo.mp4" type="video/mp4">
-			</video>
-		</div>
-	</div>
+
 
 		<div id="Maincontainer">
 
@@ -59,33 +57,110 @@
 				<a href="#">Mix</a>
 			</div>
 
-
-
 			<!--displaying all Quiz--->
 	<div class="centerBlack">News Feed</div>
+	<div class='row'>
+				<div class='section scrollspy'>
+						<div class='container'>
+								<div class='row'>
 			<?php
-				include_once("db.php");
-				$query = "SELECT * FROM quiz_list";
-				$execQuery = mysqli_query($con, $query);
-				while ($fetchQuiz = mysqli_fetch_assoc($execQuery)) {
-					$QuizCode = $fetchQuiz["quiz_code"];
-					$pic = $fetchQuiz["picture"];
-					$title = $fetchQuiz['title'];
-	        $Desc = $fetchQuiz["description"];
-	        $Cat = $fetchQuiz["categories"];
-	        $Pub = $fetchQuiz["publish"];
-	        $newDate = date("m-d-Y", strtotime($Pub));
-
-					if(strlen($title) >= 1){
-		            $Ftitle = substr($title,0,25) . "...";
-
-				include_once("DisplayQuiz.php");
+			include_once("db.php");
+			$query = "SELECT * FROM quiz_list";
+			$execQuery = mysqli_query($con, $query);
+			while ($fetchQuiz = mysqli_fetch_assoc($execQuery)) {
+				$QuizCode = $fetchQuiz["quiz_code"];
+				$pic = $fetchQuiz["picture"];
+				$title = $fetchQuiz['title'];
+				$Desc = $fetchQuiz["description"];
+				$Cat = $fetchQuiz["categories"];
+				$Pub = $fetchQuiz["publish"];
+				$newDate = date("m-d-Y", strtotime($Pub));
 
 
+				if(strlen($title) >= 1){
+							$Ftitle = substr($title,0,25) . "...";
+
+							if ($Cat == "Educational") {
+							echo "
+									<div class='col s12 m4 l4'>
+										<div class='card'>
+											<div class='card-image waves-effect waves-block waves-light'>
+												<img class='activator' src='res/quizPicture/$pic' height='200px'>
+											</div>
+
+											<div class='card-content'>
+												<span class='card-title activator grey-text text-darken-4'>$title<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
+												<p><a href='#'>Play quiz</a></p>
+											</div>
+
+											<div class='card-reveal'>
+												<span class='card-title grey-text text-darken-4'>$title<i class='mdi-navigation-close right material-icons'>close</i></span>
+													<p>Description: $Desc</p>
+													<p>Date: $newDate</p>
+													<p>Category: $Cat</p>
+											</div>
+										</div>
+									</div>
+							";
+							}
+
+					if ($Cat == "Entertainment") {
+
+					echo "
+					<div class='col s12 m4 l4'>
+							<div class='card'>
+									<div class='card-image waves-effect waves-block waves-light'>
+											<img class='activator' src='res/quizPicture/$pic' height='200px'>
+									</div>
+									<div class='card-content'>
+											<span class='card-title activator grey-text text-darken-4'>$title<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
+											<p><a href='#'>Play quiz</a></p>
+									</div>
+									<div class='card-reveal'>
+											<span class='card-title grey-text text-darken-4'>$title<i class='mdi-navigation-close right material-icons'>close</i></span>
+											<p>Description: $Desc</p>
+											<p>Date: $newDate</p>
+											<p>Category: $Cat</p>
+									</div>
+							</div>
+					</div>
+
+					";
+					}
+
+					if ($Cat == "Mix") {
+					echo "
+					<div class='col s12 m4 l4'>
+						<div class='card'>
+							<div class='card-image waves-effect waves-block waves-light'>
+								<img class='activator' src='res/quizPicture/$pic' height='200px'>
+							</div>
+
+							<div class='card-content'>
+								<span class='card-title activator grey-text text-darken-4'>$title<i class='mdi-navigation-more-vert right material-icons'>more_vert</i></span>
+									<p><a href='#'>Play Quiz</a></p>
+							</div>
+
+							<div class='card-reveal'>
+								<span class='card-title grey-text text-darken-4'>$title<i class='mdi-navigation-close right material-icons'>close</i></span>
+								<p>Description: $Desc</p>
+								<p>Date: $newDate</p>
+								<p>Category: $Cat</p>
+							</div>
+						</div>
+				</div>
+
+					";
+				}
 				}
 			}
 			?>
-
+		</div>
+		</div>
+		</div>
+		</div>
+		<script type="text/javascript" src="css/script/jquery-3.1.1.min.js"></script>
+		<script type="text/javascript" src="css/script/materialize.min.js"></script>
 		</div>
 
 
