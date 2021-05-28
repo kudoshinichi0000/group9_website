5ece6a6f7eed9d1c974087daf23424f7dedb74b8
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-29 03:18:16 +0800

    cancel

--- TrueOrFalse.php
+++ TrueOrFalse.php
@@ -61,7 +61,7 @@
                         <div class="row form-group" style="margin-top: 40px;">
                         <div class="col">
                           <button type="submit" name="btn" class="btn btn-outline-info float-right" style="margin-left:15px;"value="Submit">Submit</button>
-                          <?php echo "<a href="viewadminuser.php" class="btn btn-outline-danger">Cancel</a>";?>
+                          <?php echo "<a href='questions.php?quiz_code=$code' class='btn btn-outline-danger float-right'>Cancel</a>";?>
                         <input type="hidden" name="id" value="<?php echo $userId ?>">
                         </div>
                       </div>
