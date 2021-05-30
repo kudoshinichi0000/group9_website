5e14d54bcfbe0e47553c4037078bbb53b09d283b
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-30 18:37:41 +0800

    g

--- TrueOrFalse.php
+++ TrueOrFalse.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <script src="js/bootstrap.bundle.min.js"> </script>
+    <link type="text/css" rel="stylesheet" href="css/card.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min4.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
