c9661d7c220ad97c1f97bed72da47b1a86971b2d
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-30 19:21:38 +0800

    g

--- quiz_list.php
+++ quiz_list.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Quiz list</title>
     <link rel="stylesheet" type="text/css" href="css/createQuiz.css">
+    <link type="text/css" rel="stylesheet" href="css/card.css">
     <script src="js/bootstrap.bundle.min.js"> </script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -151,5 +152,5 @@
       });
     </script>
   </body>
-  
+
 </html>
