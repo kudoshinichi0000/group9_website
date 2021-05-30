bc986a30fb3ab3f71aff55c1cc91c0d7e13be470
Princess Angel <princess.raynaud0808@gmail.com>
2021-05-30 17:54:25 +0800

    shuta gumana ka

--- addMultipleQuestion.php
+++ addMultipleQuestion.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Creating Multiple Question</title>
+    <link type="text/css" rel="stylesheet" href="css/card.css">
     <script src="js/bootstrap.bundle.min.js"> </script>
       <link rel="stylesheet" type="text/css" href="css/bootstrap.min4.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- viewadminuser.php
+++ viewadminuser.php
@@ -18,7 +18,7 @@ $fetchid = $admin['userid'];
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min3.css">
-
+    <link type="text/css" rel="stylesheet" href="css/card.css">
     <script src="js/bootstrap.bundle.min.js"> </script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
