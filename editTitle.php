67110540c331f5790de6f9f22f3b8821f0de70ea
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 16:14:37 +0800

    h

--- editTitle.php
+++ editTitle.php
@@ -2,13 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <script src="js/bootstrap.bundle.min.js"> </script>
-    <link type="text/css" rel="stylesheet" href="css/card.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min4.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <style media="screen">
+    <link type="text/css" rel="stylesheet" href="css/navbar.css">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0;">
+  	<link rel="preconnect" href="https://fonts.gstatic.com">
+  	<link href="https://fonts.googleapis.com/css2?family=Orelega+One&display=swap" rel="stylesheet">
+  	<link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" crossorigin="anonymous">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<style media="screen">
+
 
     .card{
       width: 50%;
@@ -131,6 +132,6 @@
             </div>
           </div>
         </div>
-            
+
   </body>
 </html>
