8f21bfb1eccf5a6f23bffe9bc9059044c7fb461d
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 16:11:04 +0800

    d

--- editTitle.php
+++ editTitle.php
@@ -8,6 +8,21 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <style media="screen">
+    .card{
+      width: 80%;
+      height: 100%;
+      margin-top: 1rem;
+      margin-bottom: 5rem;
+    }
+
+    .jumbotron{
+
+      height: 100%;
+      margin-bottom: 15px;
+    }
+
+    </style>
     <title>Edit Title</title>
   </head>
   <body>
@@ -115,8 +130,8 @@
             </div>
           </div>
         </div>
-              ";
+            ";
 
-  ?>
+              ?>
   </body>
 </html>
