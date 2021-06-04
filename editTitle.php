d0d62b9d79f5f199b7d0051413d47dd65a102f76
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 16:21:17 +0800

    d

--- editTitle.php
+++ editTitle.php
@@ -8,23 +8,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  
-    <style media="screen">
 
-    .card{
-      width: 50%;
-      height: 60%;
-      margin-top: 1rem;
-      margin-bottom: 5rem;
-    }
-
-    .jumbotron{
-      width: 60%;
-      height: 100%;
-      margin-bottom: 15px;
-    }
-
-    </style>
     <title>Edit Title</title>
   </head>
   <body>
@@ -49,7 +33,7 @@
     $picture = $fetchCodes["picture"];
 
         echo "
-        <div class='container>
+        <div style='width: 80%;' class='container>
           <div class='card'>
             <div class='card-header'>
               <h2 style='margin-left:30%'><b> Edit Title Quiz<b></h2>
