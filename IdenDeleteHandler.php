b8b619511fe64fdfdb01413ba7338903d099cca9
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 21:30:20 +0800

    h

--- IdenDeleteHandler.php
+++ IdenDeleteHandler.php
@@ -5,53 +5,6 @@
     <title></title>
   </head>
   <body>
-
-  <?php
-  $quizId = $_GET['id'];
-
-  $query = "SELECT * FROM identification WHERE id = '$quizId'";
-
-  $execQuery = mysqli_query($con, $query);
-
-  $fetchCodes = mysqli_fetch_assoc($execQuery);
-  $question = $fetchCodes["question"];
-  $code = $fetchCodes["quiz_code"];
-  $questionP = $fetchCodes["points"];
-
-  echo"
-  <div class='modal fade' id='DeleteModalIden'>
-    <div class='modal-dialog'>
-        <div class='modal-content'>
-          <div class='modal-header'>
-            <h2 class='modal-title'> Delete Confirmation</h2>
-            <button type='button' class='close' data-dismiss='modal'><span>&times;</span></button>
-          </div>
-          <div class='modal-body'>
-              <form action='IdenDeleteHandler.php' id='form-delete-user' method='POST'>
-                <label><p>  Are you sure you want to delete your account: <?php echo $question?>?</p></label>
-
-          </div>
-          <div class='modal-footer'>
-            <input type='submit' name='Confirm' class='buttonnn button01' value='yes'>
-              <input type='submit' name='Confirm' class='buttonnn button1' value='no'>
-          </div>
-          <input type='hidden' name='quizId' value='$quizId'>
-          <input type='hidden' name='quizCode' value='$code'>
-          <input type='hidden' name='quizP' value='$questionP'>
-          </form>
-      </div>
-    </div>
-  </div>
-  <script>
-    $('.delete-btn').click((e) => {
-      e.preventDefault();
-      if(confirm(' Are you sure you want to delete this item')){
-        window.location.href = $(e.target).attr('href');
-      }
-    });
-  </script>
-  "
-?>
   <?php
   include_once("db.php");
 	//Var_dump
