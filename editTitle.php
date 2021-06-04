bfb33cb6e6b13180d3feaf88b8046f15a487768e
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 16:02:49 +0800

    f

--- editTitle.php
+++ editTitle.php
@@ -83,6 +83,7 @@
                                 <option value='Entertainment'>Entertainment</option>
                                 ";
                               }
+                            }
                             ?>
                             </select>
                           </div>
