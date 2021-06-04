c7e5e4251496037c456a5a085cc760e08944c844
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 16:12:09 +0800

    try

--- editTitle.php
+++ editTitle.php
@@ -9,9 +9,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <style media="screen">
+
     .card{
-      width: 80%;
-      height: 100%;
+      width: 50%;
+      height: 60%;
       margin-top: 1rem;
       margin-bottom: 5rem;
     }
