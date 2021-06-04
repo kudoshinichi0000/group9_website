08197f9fe836b857d39be7863765497040da2157
agapito <agapitojezreel25@gmail.com>
2021-06-04 23:52:11 +0800

    Pagination

--- main.php
+++ main.php
@@ -8,8 +8,6 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Orelega+One&display=swap" rel="stylesheet">
 	<link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 
 </head>
 <body>
@@ -29,7 +27,7 @@
 
 	<!---Welcoming text--->
 	<div class="Maincontainer"><br>
-		<b style="font-size: 4em; ">BuzzFeed Quizzes</b>
+		<b style="font-size: 4em; ">Feedopedia Quizzes</b>
 		<p>We've got all the quizzes you love to binge! Come on in and hunker down for the long haul.</p>
 
 		<!---Search Button--->
@@ -50,51 +48,82 @@
 		<!--displaying all Quiz--->
 		<div class="centerBlack">News Feed</div><br>
 			<?php
-				include_once("db.php");
-				$query = "SELECT * FROM quiz_list ORDER BY publish DESC";
-				$execQuery = mysqli_query($con, $query);
-				while ($fetchQuiz = mysqli_fetch_assoc($execQuery)) {
-					$QuizCode = $fetchQuiz["quiz_code"];
-					$pic = $fetchQuiz["picture"];
-					$title = $fetchQuiz['title'];
-	        $Desc = $fetchQuiz["description"];
-	        $Cat = $fetchQuiz["categories"];
-	        $Item = $fetchQuiz["items"];
-					$Os = $fetchQuiz["OverallScores"];
+			$db = new PDO( 'mysql:dbname=quizdb;host=127.0.0.1','root','' );
+
+			// user input
+			$page 	 = isset( $_GET['page'] ) ? (int) $_GET['page'] : 1;
+			$perPage = isset( $_GET['per-page'] ) && $_GET['per-page'] <= 50 ? (int) $_GET['per-page'] : 12;
+
+			// positioning
+			$start = ( $page > 1 ) ? ( $page * $perPage ) - $perPage : 0;
+
+			// query
+			$articles = $db->prepare( "SELECT SQL_CALC_FOUND_ROWS * FROM quiz_list LIMIT {$start}, {$perPage}" );
+			$articles->execute();
+			$articles = $articles->fetchAll( PDO::FETCH_ASSOC );
+
+			// pages
+			$total = $db->query( "SELECT FOUND_ROWS() as total" )->fetch()['total'];
+			$pages = ceil( $total / $perPage );
 
 
-					if($Ftitle = wordwrap($title, 25, "<br>")) {
-
+			//if($Ftitle = wordwrap($articles["title"], 25, "<br>")) {
+			foreach ( $articles as $article ):
+			$QuizCode = $article["quiz_code"];
 					echo "
 					<div class='align'>
 					<div class='card-container'>
 						<div class='card-horizontal'>
 							<div class='card-front'>
 								<article class='card-front-content'>
-								<img src='res/quizPicture/$pic' width='100%' height='175px' alt='' class='card-pic'>
-								<h2 style='color:#fff;'>$Ftitle</h2>
+								<img src='res/quizPicture/$article[picture]' width='100%' height='175px' alt='' class='card-pic'>
+								<h2 style='color:#fff;'>$article[title]</h2>
 									</article>
 							</div>
 							<div class='card-back card-back-hr'>
 								<article class='card-back-content'>
-								<h2>Title: $Ftitle</h2>
-								<p style='color:#fff;'>Category: $Cat</p>
-								<p style='color:#fff;'>Items: $Item</p>
-								<p style='color:#fff;'>Overall Scores: $Os</p>
-								<a class='PlayButton' href='Displayinfo.php?quiz_code=$QuizCode'>Play Quiz</a>
-									</article>
-
+									<h2>Title: $article[title]e</h2>
+									<p style='color:#fff;'>Category:	$article[categories]</p>
+									<p style='color:#fff;'>Items: $article[items]</p>
+									<p style='color:#fff;'>Overall Scores: $article[OverallScores]</p>
+									<a class='PlayButton' href='Displayinfo.php?quiz_code=$QuizCode'>Play Quiz</a>
+								</article>
 							</div>
 						</div>
 					</div>
 			</div>
-
-
 					";
-			}
-		} //<a href='takeQuizMultipleChoice.php?quiz_code=$QuizCode'>Play Quiz
+			 endforeach;
+
+			 ?>
+
+			 </div>
+
+			 <?php
+				 echo "<br><br><br>
+				 <div class='col-md-12'>
+			 <div class='well well-sm'>
+				 <div class='paginate'>
+				 ";
+					 for ( $x=1; $x <= $pages; $x++ ):
+						 echo "
+					 <ul class='pagination'>
+						 <li>
+							 <a href='?page=$x; &per-page=$perPage'>$x</a>
+						 </li>
+					 </ul>
+					 ";
+					  endfor;
+						echo "
+				 </div>
+			 </div>
+		 </div>
+
+				 ";
+
+			//}
+
 			?>
-		</div>
 
 
 
