f1558ed9f5df3f9e468b3f12c5d7f33ce2c1ded9
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 16:26:29 +0800

    d

--- editTitle.php
+++ editTitle.php
@@ -33,7 +33,7 @@
     $picture = $fetchCodes["picture"];
 
         echo "
-        <div style='width: 90%; margin-left:5rem;' class='container>
+        <div style='width: 90%; margin-left:4rem;' class='container>
           <div class='jumbotron'>
           <div class='card'>
             <div class='card-header'>
@@ -118,8 +118,6 @@
           </div>
           </div>
         </div>
-            ";
-
-              ?>
+            
   </body>
 </html>
