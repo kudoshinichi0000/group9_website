114e4fa3e8b4cd107da76ffb3231e1b6fdb09829
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 20:42:56 +0800

    d

--- deletetrueorfalse.php
+++ deletetrueorfalse.php
@@ -97,7 +97,24 @@
         }
       });
     </script>
-    
+    <form action='deletetrueorfalseHandler.php' method='POST'>
+      <table border='1' height='350px' width='25%' class='container1'>
+        <tr>
+          <th colspan='2'><h2>Are You Sure you want to delete this item?</h2>
+          <h2>$question</h2></th>
+        </tr>
+        <tr>
+          <th colspan='2'><input type='submit' name='Confirm' value='yes'>
+          <input type='submit' name='Confirm' value='no'></th>
+        </tr>
+        <tr>
+          <th colspan='2'><a href='questions.php?quiz_code=$code'>Cancel</a></th>
+        </tr>
+      </table>
+      <input type='hidden' name='quizId' value='$quizId'>
+      <input type='hidden' name='quizCode' value='$code'>
+      <input type='hidden' name='quizP' value='$questionP'>
+    </form>
     ";
 
      ?>
