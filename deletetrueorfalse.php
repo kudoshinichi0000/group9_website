5fe082f6ba9e9c8db3bf84e9718a1a8bfcac12a4
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 20:33:31 +0800

    try

--- deletetrueorfalse.php
+++ deletetrueorfalse.php
@@ -2,7 +2,43 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Delete Question</title>
+    <!--  css for the message alert delete button options, yes or no -->
+    <style>
+    .buttonnn{
+      border:none;
+      color:white;
+      padding: 10px 30px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 18px;
+      margin:4px 2px;
+      transition-duration: 0.2s;
+      cursor: pointer;
+
+    }
+
+    .button01{
+      background-color: white;
+      color:black;
+      border: 2px solid #f44336
+    }
+    .button01:hover{
+        background-color: #f44336;
+        color: white;
+
+      }
+    .button1{
+        background-color:white;
+        color:black;
+        border: 2px solid #555555;
+      }
+    .button1:hover{
+          background-color: #555555;
+          color: white;
+      }
+    </style>
   </head>
   <body>
     <?php
@@ -20,6 +56,38 @@
     $questionP = $fetchCodes["points"];
 
     echo "
+
+    <div class='modal fade' id='DeleteModal'>
+      <div class='modal-dialog'>
+          <div class='modal-content'>
+            <div class='modal-header'>
+              <h2 class='modal-title'> Delete Confirmation</h2>
+              <button type='button' class='close' data-dismiss='modal'><span>&times;</span></button>
+            </div>
+            <div class='modal-body'>
+                <form action='deletetrueorfalseHandler.php' id='form-delete-user' method='POST'>
+                  <label><p>  Are you sure you want to delete your account: <?php echo $question?>?</p></label>
+
+            </div>
+            <div class='modal-footer'>
+              <input type='submit' name='choice' class='buttonnn button01' value='yes'>
+                <input type='submit' name='choice' class='buttonnn button1' value='no'>
+            </div>
+            <input type='hidden' name='quizId' value='$quizId'>
+            <input type='hidden' name='quizCode' value='$code'>
+            <input type='hidden' name='quizP' value='$questionP'>
+            </form>
+        </div>
+      </div>
+    </div>
+    <script>
+      $('.delete-btn').click((e) => {
+        e.preventDefault();
+        if(confirm(' Are you sure you want to delete this item')){
+          window.location.href = $(e.target).attr('href');
+        }
+      });
+    </script>
     <form action='deletetrueorfalseHandler.php' method='POST'>
       <table border='1' height='350px' width='25%' class='container1'>
         <tr>
