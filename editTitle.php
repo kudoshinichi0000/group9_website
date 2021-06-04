8ecd8e90b6b1a7f1a3443b30d53f7fa5efecb8ce
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-04 14:08:56 +0800

    edit

--- editTitle.php
+++ editTitle.php
@@ -8,7 +8,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <title></title>
+    <title>Edit Title</title>
   </head>
   <body>
     <?php
@@ -32,6 +32,13 @@
     $picture = $fetchCodes["picture"];
 
         echo "
+        <div class='container>
+          <div class='card'>
+            <div class='card-header'>
+              <h2 style='margin-left:30%'><b> Edit Title Quiz<b></h2>
+            </div>
+          </div>
+        </div>
         <div class='container1'>
     		<form action='editTitleHandler.php' method='POST' enctype='multipart/form-data'>
     			<table border='1' height='350px' width='35%' class='container1'>
