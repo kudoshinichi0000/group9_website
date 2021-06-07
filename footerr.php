d7750afb28958b23bd890df976d044db53f6539c
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-07 08:39:36 +0800

    tr

--- footerr.php
+++ footerr.php
@@ -22,6 +22,11 @@
       color: white;
       font-size: 16px;
     }
+    .social-icons ul{
+      padding:0;
+
+
+    }
     .footer-top{
       background: #000000;
       padding: 10px 0;
@@ -162,11 +167,13 @@
               <div class="col-md-4 col-sm-6 col-xs-12 segment-three md-mb-30 sm-mb-30">
                   <h2> Contacts</h2>
                   <p> You can visit out social media accounts for transactions</p>
-                  <<ul>
-                    <li><a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-instagram"></i></a>
-                    </li><a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-gmail"></i></a></li>
-                </ul>
+                  <div class="social-icons">
+                    <ul>
+                      <li><a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-facebook"></i></a></li>
+                      <li><a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-instagram"></i></a>
+                      </li><a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-gmail"></i></a></li>
+                    </ul>
+                  </div>
               </div>
 
             </div>
