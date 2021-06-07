e4dde14d74b18e263b333899e965ff1b9ae1529d
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-07 08:32:36 +0800

    HAHA

--- footerr.php
+++ footerr.php
@@ -159,7 +159,7 @@
                       <li><a href="#"></a> Policy</li>
                   </ul>
               </div>
-              <div class="col-md-5 col-sm-6 col-xs-12 segment-three md-mb-30 sm-mb-30">
+              <div class="col-md-4.5 col-sm-6 col-xs-12 segment-three md-mb-30 sm-mb-30">
                   <h2> Contacts</h2>
                   <p> You can visit out social media accounts for transactions</p>
                   <a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-facebook"></i></a>
