8a291a3ff216d8f1e3f214b73300da5c20e51034
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-07 08:37:25 +0800

    fa

--- footerr.php
+++ footerr.php
@@ -162,7 +162,11 @@
               <div class="col-md-4 col-sm-6 col-xs-12 segment-three md-mb-30 sm-mb-30">
                   <h2> Contacts</h2>
                   <p> You can visit out social media accounts for transactions</p>
-                  <a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-facebook"></i></a>
+                  <<ul>
+                    <li><a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-instagram"></i></a>
+                    </li><a href="https://www.facebook.com/JezreelAgapito27/"><i class="fa fa-gmail"></i></a></li>
+                </ul>
               </div>
 
             </div>
