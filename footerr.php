fd426d176f923c477a64c3b2394e543767ef7042
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-07 08:26:54 +0800

    gg

--- footerr.php
+++ footerr.php
@@ -149,7 +149,7 @@
                   <h3> Feedopedia</h3>
                   <p> Feedopedia is Recreation of knowledgeable and fun buzzfeed quizzes Activities for all ages.</p>
               </div>
-              <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+              <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
                   <h2> LINKS LINKS</h2>
                   <ul>
                       <li><a href="#"></a> Services</li>
