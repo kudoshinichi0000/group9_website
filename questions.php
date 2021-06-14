b84ed82d6d2a0fd3a2bc6f84ae3d6d13e8e6a6e4
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-14 20:25:16 +0800

    try

--- questions.php
+++ questions.php
@@ -113,6 +113,33 @@
     //displaying Buttons
     echo "
       <br><br>
+
+			<div>
+			<button class='view-modal'>Click Me</button>
+<div class='popup'>
+  <header>
+    <span>Share Modal</span>
+    <div class='close'><i class='uil uil-times'></i></div>
+  </header>
+  <div class='content'>
+    <p>Share this link</p>
+    <ul class='icons'>
+      <a href='#'>=<i class='bi bi-file-check-fill'></i></a>
+      <a href='#'><i class='fab fa-facebook'></i></a>
+      <a href='#'><i class='fab fa-github'></i></a>
+    </ul>
+    <p>Click to Copy</p>
+    <div class='field'>
+      <i class='url-icon uil uil-link'></i>
+      <input type ='text' readyonly value ='example.com/share-link'>
+      <button>Copy</button>
+    </div>
+  </div>
+</div>
+			</div>
+
+
+
 			<div>
 			<button class='view-modal1'> Add Questions </button>
 			<div class='popupm'>
