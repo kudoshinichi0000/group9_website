1a2e519f2904c2d05d3351ef899f58a516baa394
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-14 21:20:36 +0800

    g

--- questions.php
+++ questions.php
@@ -126,6 +126,9 @@
       <a href='#'>=<i class='bi bi-file-check-fill'></i></a>
       <a href='#'><i class='fab fa-facebook'></i></a>
       <a href='#'><i class='fab fa-github'></i></a>
+			<a href='#'><i class='fas fa-comments'></i></a></li>
+				<a href='#'><i class='fas fa-check-square'></i></i></a>
+			 <a href='#'='><i class='fas fa-minus-square'></i></a>
     </ul>
     <p>Click to Copy</p>
     <div class='field'>
