e036c429760f76b8c8582468cc9498a18a6e6f38
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-15 00:33:30 +0800

    g

--- questions.php
+++ questions.php
@@ -18,6 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min3.css">
     <link type="text/css" rel="stylesheet" href="css/card.css">
+		<link type="text/css" rel="stylesheet" href="css/popup.css">
     <script src="js/bootstrap.bundle.min.js"> </script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -325,6 +326,6 @@
    });
  </script>
  -->
- <script src="js/popup.js"> </script>
+ <script src="script/popup.js"> </script>
   </body>
 </html>
