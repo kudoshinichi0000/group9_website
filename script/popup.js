5c28ab40510e9cee4c4d3fa7325a7a1c1c8da2e1
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-15 18:30:08 +0800

    try

--- script/popup.js
+++ script/popup.js
@@ -1,9 +1,6 @@
 const viewBtn = document.querySelector(".view-modaal"),
     popuup = document.querySelector(".popuup"),
     close = popuup.querySelector(".close"),
-    field = popuup.querySelector(".field"),
-    input = field.querySelector("input"),
-    copy = field.querySelector("button");
 
     viewBtn.onclick = ()=>{
       popuup.classList.toggle("show");
@@ -12,15 +9,5 @@ const viewBtn = document.querySelector(".view-modaal"),
       viewBtn.click();
     }
 
-    copy.onclick = ()=>{
-      input.select(); //select input value
-      if(document.execCommand("copy")){ //if the selected text copy
-        field.classList.add("active");
-        copy.innerText = "Copied";
-        setTimeout(()=>{
-          window.getSelection().removeAllRanges(); //remove selection from document
-          field.classList.remove("active");
-          copy.innerText = "Copy";
-        }, 3000);
-      }
+
     }
