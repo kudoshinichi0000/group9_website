d0605d71b5c2e0170d9ee7d24a3b14268c2b0eb7
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-15 11:42:16 +0800

    d

---
+++ popup.php
@@ -0,0 +1,39 @@
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <script src="script/popupmodal.js"> </script>
+    <link type="text/css" rel="stylesheet" href="css/card.css">
+  <title></title>
+</head>
+<body>
+  <button class="view-modal">Click Me</button>
+<div class="popup">
+  <header>
+    <span>ADD QUESTIONS</span>
+    <div class="close"><i class="uil uil-times"></i></div>
+  </header>
+  <div class="content">
+    <p>QUIZ EDITOR</p>
+    <ul class="icons">
+      <a href="#">=<i class="bi bi-file-check-fill"></i></a>
+      <a href="#"><i class="bi bi-dash-square"></i></a>
+      <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+      <a href="#"><i class="fab fa-facebook"></i></a>
+      <a href="#"><i class="fab fa-github"></i></a>
+    </ul>
+
+  </div>
+</div>
+
+
+
+
+</body>
+</html>
