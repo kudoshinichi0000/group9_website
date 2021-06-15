1d0e3708373472d6350b84afbf0de519d7ff107c
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-15 21:11:05 +0800

    pota ayaw na naman

--- script/popup.js
+++ script/popup.js
@@ -1,6 +1,7 @@
 const viewBtn = document.querySelector(".view-modaal"),
     popuup = document.querySelector(".popuup"),
     close = popuup.querySelector(".close"),
+    field = popup.querySelector(".field"),
     input = field.querySelector("input"),
     copy = field.querySelector("button");
 
@@ -11,4 +12,15 @@ const viewBtn = document.querySelector(".view-modaal"),
       viewBtn.click();
     }
 
-    
+    copy.onclick = ()=>{
+      input.select(); //select input value
+      if(document.execCommand("copy")){ //if the selected text copy
+        field.classList.add("active");
+        copy.innerText = "Copied";
+        setTimeout(()=>{
+          window.getSelection().removeAllRanges(); //remove selection from document
+          field.classList.remove("active");
+          copy.innerText = "Copy";
+        }, 3000);
+      }
+    }
