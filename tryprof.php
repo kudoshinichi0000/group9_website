ae3567b7b0447e7a95550a6c869cbc743fbe9562
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-24 14:10:09 +0800

    f

--- tryprof.php
+++ tryprof.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0;">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    
 
 
   	<!---this link is to design the font style <link rel="stylesheet" type="text/css" href="css/profile.css">--->
