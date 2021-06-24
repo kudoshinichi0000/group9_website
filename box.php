0dc848dd302ce35ba89d16adf4ef03a305103a3d
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-24 09:20:04 +0800

    d

--- box.php
+++ box.php
@@ -1,7 +1,29 @@
-
+<html>
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="i.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Welcome User!</title>
 </head>
+<body>
+  <div class="container">
+    <div class="heading">
+      <h1>welcome User!</h1>
+    </div>
+    <div class="row">
+      <div class="card">
+        <div class="card-header">
+          <h1>React</h1>
+        </div>
+        <div class="card-body">
+          <p>
+            userid :jflk,sjgm;flgdf
+            <br>
+            username: kfjsdlkgmflg;fd
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
