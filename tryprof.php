074eaea1f5874fd1263b0292217a472bdcde7db6
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-24 14:34:09 +0800

    f

--- tryprof.php
+++ tryprof.php
@@ -194,6 +194,106 @@
       box-shadow: none;
       line-height: none;
     }
+    .badge-info{
+      color:#fff;background-color:#17a2b8
+      }
+      .badge-info[href]:focus,.badge-info[href]:hover{
+        color:#fff;text-decoration:none;
+        background-color:#117a8b;
+      }
+      .badge-danger{
+        color:#fff;background-color:#dc3545
+        }
+        .badge-danger[href]:focus,.badge-danger[href]:hover{
+          color:#fff;text-decoration:none;background-color:#bd2130;
+        }
+
+    :root {
+--modal-duration: 1s;
+--modal-color: #428bca;
+}
+
+body {
+font-family: Arial, Helvetica, sans-serif;
+background: #f4f4f4;
+font-size: 17px;
+line-height: 1.6;s
+display: flex;
+height: 100vh;
+align-items: center;
+justify-content: center;
+}
+
+.modal {
+display: none;
+position: fixed;
+z-index: 1;
+left: 0;
+top: 0;
+height: 100%;
+width: 100%;
+overflow: auto;
+background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+margin: 10% auto;
+width: 60%;
+box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 7px 20px 0 rgba(0, 0, 0, 0.17);
+animation-name: modalopen;
+animation-duration: var(--modal-duration);
+}
+
+.modal-header h2,
+.modal-footer h3 {
+margin: 0;
+}
+
+.modal-header {
+background: var(--modal-color);
+padding: 15px;
+color: #fff;
+border-top-left-radius: 5px;
+border-top-right-radius: 5px;
+}
+
+.modal-body {
+padding: 10px 20px;
+background: #fff;
+}
+
+.modal-footer {
+background: var(--modal-color);
+padding: 10px;
+color: #fff;
+text-align: center;
+border-bottom-left-radius: 5px;
+border-bottom-right-radius: 5px;
+}
+
+.close {
+color: #ccc;
+float: right;
+font-size: 30px;
+color: #fff;
+}
+
+.close:hover,
+.close:focus {
+color: #000;
+text-decoration: none;
+cursor: pointer;
+}
+
+@keyframes modalopen {
+from {
+opacity: 0;
+}
+to {
+opacity: 1;
+}
+}
+
 
     </style>
   </head>
@@ -242,7 +342,9 @@
 					UserId:</p>
       </div>
       <div class="modal-footer">
-        <h3>Modal Footer</h3>
+        <h3>
+          <button type='button' data-toggle='modal' data-target='#DeleteModal'class='btn btn-danger badge-pill text-right float-right' style='width:80px; text-align:center; margin:5px;'>DELETE</button>
+          <a href='editadmin.php?id={$fetchid}' type='button' class='btn btn-info badge-pill text-centered float-right' style='width:80px; text-align: center; margin:5px;'>EDIT</a>Modal Footer</h3>
       </div>
     </div>
   </div>
