f20d6a2d43f3608fc0867452e300612895d2861a
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-24 14:01:41 +0800

    modal

--- tryprof.php
+++ tryprof.php
@@ -229,6 +229,22 @@
   </div>
 </nav>
 <div id="my-modal" class="modal">
+  <div class="modal-content">
+      <div class="modal-header">
+        <span class="close">&times;</span>
+        <h2>Modal Header</h2>
+      </div>
+      <div class="modal-body">
+        <p>This is my modal</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla repellendus nisi, sunt consectetur ipsa velit
+          repudiandae aperiam modi quisquam nihil nam asperiores doloremque mollitia dolor deleniti quibusdam nemo
+          commodi ab.</p>
+      </div>
+      <div class="modal-footer">
+        <h3>Modal Footer</h3>
+      </div>
+    </div>
+  </div>
 
 <script>
   function menuToggle(){
