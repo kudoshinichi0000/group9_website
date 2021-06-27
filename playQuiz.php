30ee6cb5b9e39fc5f7245a7835d74c4d126d01d7
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-27 17:41:13 +0800

    D

--- playQuiz.php
+++ playQuiz.php
@@ -22,3 +22,32 @@
 	$total_questions = mysqli_num_rows(mysqli_query($con,$query));
  include_once "navbar2.php"
 ?>
+<html>
+<head>
+	<title>Feedopedia Play Quiz</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+</head>
+<body>
+	</main>
+
+<div class="wrapper">
+      <div class="quiz">
+        <div class="quiz_header">
+          <div class="quiz_user">
+          	<div class="current"><h4><span class="name">Question <?php echo $number; ?> of <?php echo $total_questions; ?></span></h4> </div>
+          </div>
+          <div class="quiz_timer">
+            <span class="time">goodluck</span>
+          </div>
+        </div>
+        <div class="quiz_body">
+          <div id="questions">
+
+
+
+				<!---Questions--->
+				<p class="question">Question: <?php echo $question['question']; ?> </p>
+
+				<form method="POST" action="process.php">
+					<ul class="choicess">
