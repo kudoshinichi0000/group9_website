c3abf46619823ac8807997593ca9e95be583785c
Princess Angel <princess.raynaud0808@gmail.com>
2021-06-27 17:47:12 +0800

    CSS

--- quizResult.php
+++ quizResult.php
@@ -2,71 +2,57 @@
 
 include_once("db.php");
 
+
 ?>
 
 <html>
-<head>
+<head>>
 	<title>PHP Quizer</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	 <style>
+      body{
+        margin: 0;
+        padding:0;
+        text-align: center;
+        font-family: sans-serif;
+        /*background-image: url("res/images/fb10.png");*/
+        background-image: linear-gradient(to left top, #1a8e74, #148d6e, #0f8c68, #0d8b61, #0e8a5a, #008d6e, #008f7f, #00908d, #0091b4, #008ddf, #007ef8, #9a5bed);
+      	height:auto;
+      	width: auto;
+      	background-position: center;
+      	background-repeat: no-repeat;
+      	background-size: cover;
+      	text-align: center;
+      	padding:10px;
+      }
+      .container-feedback-title{
+        margin-top: 95px;
+        color:#fff;
+        text-transform: uppercase;
+        transition: all 4s ease-in-out;
+      }
+      .conatiner-feedback-title h1{
 
-	<header>
-		<div class="container">
-		</div>
-	</header>
-
-	<main>
-			<div class="container">
-				<h2>Your Result</h2>
-				<p>Congratulation You have completed this test succesfully.</p>
-				<p>Your <strong>Score</strong> is <?php echo $_SESSION['score']; ?>  </p>
-			</div>
-	</main>
-
-	<form class="" action="quizResult.php" method="post">
-		<label for="name">Full name: </label>
-		<input type="text" name="name" required><br>
-
-		<label for="email">email: </label>
-		<input type="email" name="email" required><br>
-
-		<label for="Feedback">Feedback: </label>
-		<textarea  name="Feedback" required></textarea><br>
-
-		<input type="submit" name="submit" >
-	</form>
-
-
-</body>
-</html>
-
-<?php
-//Including database
-include_once("db.php");
-
-	//If the user/admin click the submit button, all of the informations in inputbox will process here, to put in database
-	if(isset($_POST['submit'])){
-
-		//Vardump
-		$score = $_SESSION['score'];
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$Feedback = $_POST['Feedback'];
-
-		//Inserting feedback into website_feedback table
-		$query = "INSERT INTO quiz_feedback (name, email, feedback, score)
-		VALUES ('$name', '$email','$Feedback', '$score')";
-
-		//perform the query
-		$result = mysqli_query($con,$query);
-
-		if ($result) {
-			unset($_SESSION['score']);
-			echo "<br><br>Successfuly added to database
-				<a href='main.php'>Back</a>
-			";
-
-		}
-	}
-	 ?>
+      font-size: 40px;
+      line-height: 10px;
+      }
+      .container-feedback-title h2{
+      font-size: 20px;
+      }
+      ::placeholder {
+        color: #ffffff;
+        opacity: 1; /* Firefox */
+      }
+      ::-webkit-input-placeholder{ /*Safari, Google Chrome, Opera 15+) and Microsoft Edge*/
+        color: #ffffff;
+        opacity: 1;
+      }
+      ::-moz-placeholder{ /* mozilla Firefox */
+        color: #ffffff;
+        opacity: 1;
+      }
+      :-ms-input-placeholder { /* Internet Explorer 10-11 */
+        color: #ffffff;
+        opacity: 1;
+      }
